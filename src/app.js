08b5a7353165a1ad18b122402fa19dd0750aefa6
Darian Tavana <datavana@gmail.com>
2022-09-12 12:58:35 -0400

    Setup initial MongoDB connection

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const routes = require("./routes");
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
+(async () => {
+  mongoose.connection.on("open", () =>
+    console.log("Connected to MongoDB instance")
+  );
+  const connection = await mongoose.connect(
+    `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@cs4241-a3.e1sejut.mongodb.net/?retryWrites=true&w=majority`
+  );
+})();
+
 app.use(routes);
 app.use(express.static("public"));
 
