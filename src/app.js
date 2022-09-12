38a1a597eeb15606833593fefa343787dde54079
Darian Tavana <datavana@gmail.com>
2022-09-12 12:31:09 -0400

    Crete base layout for routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(routes);
 app.listen(port, () => console.log(`Server listening on port ${port}`));
