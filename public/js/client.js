e4de4615ad76a5442be8f1d5abb9202564ddedf6
Jonathan Metcalf <jonathan.e.metcalf@gmail.com>
2022-09-13 00:59:12 -0400

    Completed backend

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,74 @@
-console.log('test')
+window.onload = () => {
+    document.getElementById('getBtn').onclick = getTask
+    document.getElementById('putBtn').onclick = putTask
+    document.getElementById('deleteBtn').onclick = deleteTask
+    document.getElementById('patchBtn').onclick = patchTask
+}
+
+function getTask(e) {
+    e.preventDefault()
+
+    fetch('/todo', {
+        method: 'GET',
+    }).then(async response => {
+        console.log(await response.json())
+    })
+
+    return false
+}
+
+function putTask(e) {
+    e.preventDefault()
+
+    let body = JSON.stringify({
+        task: 'do this',
+        creation_date: '2022-09-13T01:45',
+        due_date: '2022-09-14T23:59',
+    })
+
+    fetch('/todo', {
+        method: 'PUT',
+        headers: {"Content-Type": "application/json"},
+        body
+    }).then(async response => {
+        console.log(await response.json())
+    })
+
+    return false
+}
+
+function patchTask(e) {
+    e.preventDefault()
+
+    let id = document.getElementById('taskId').value
+
+    let body = JSON.stringify({
+        task: 'no, do that',
+        due_date: '2022-09-12T10:00',
+    })
+
+    fetch('/todo/' + id, {
+        method: 'PATCH',
+        headers: {"Content-Type": "application/json"},
+        body
+    }).then(async response => {
+        console.log(await response.json())
+    })
+
+    return false
+}
+
+function deleteTask(e) {
+    e.preventDefault()
+
+    let id = document.getElementById('taskId').value
+
+    fetch('/todo/' + id, {
+        method: 'DELETE',
+    }).then(async response => {
+        console.log(await response.json())
+    })
+
+    return false
+}
+
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express'), path = require('path'), passport = require('
 require('./passport')
 
 const app = express()
-app.use(session({secret: 'secret?', resave: false, saveUninitialized: false}))
+app.use(session({secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false}))
 app.use(passport.initialize({}))
 app.use(passport.session({}))
 app.use(logger)
@@ -13,38 +13,18 @@ app.use(ensureAuthenticated)
 app.use(express.json())
 app.use(express.static(__dirname + '/public'))
 
-function logger(req, res, next) {
-    console.log('URL:', req.path)
-    return next()
-}
-
-function connectionChecker(req, res, next) {
-    if (collection !== undefined) return next()
-    else res.status(503).send()
-}
-
-// Allow the user to continue if they are going to an allowed page OR if they are authenticated
-const allowed = ['/login', '/auth/github', '/auth/github/callback']
-
-function ensureAuthenticated(req, res, next) {
-    if (allowed.includes(req.path) || req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/login')
-}
 
 // DATABASE INITIALIZATION
-let collection = undefined;
+
+let collection = undefined
 const mongodb = require('mongodb')
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/?retryWrites=true&w=majority`
 const client = new mongodb.MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true})
 
 client.connect().then(() => {
-    return client.db('data').collection('todos')
-}).then(_collection => {
-    collection = _collection
-    return collection.find({}).toArray()
-}).then(console.log)
+    collection = client.db('data').collection('todos')
+})
+
 
 // AUTHENTICATION
 
@@ -72,25 +52,96 @@ app.post('/logout', (req, res, next) => {
 
 // DB ACTIONS
 
-app.get('/todos', ensureAuthenticated, (req, res) => {
-    // TODO make sure it only gets ones for that given user
-    collection.find({}).toArray().then(result => res.json(result))
+// Get all tasks from the given user GitHub id
+app.get('/todo', ensureAuthenticated, (req, res) => {
+    collection.find({github_id: req.user.id}).toArray().then(result => {
+        res.json(result)
+    })
 })
 
-app.put('/submit', ensureAuthenticated, (req, res) => { // TODO update path
-    collection.insertOne(req.body).then(result => res.json(result))
+// Put in a new task but set GitHub ID first
+app.put('/todo', ensureAuthenticated, (req, res) => {
+    let task = req.body
+    task.github_id = req.user.id
+    task.priority = determinePriority(req.body.due_date)
+    collection.insertOne(req.body).then(result => {
+        res.json(result)
+    })
 })
 
-app.delete('/delete', ensureAuthenticated, (req, res) => { // TODO update path
-    collection
-        .deleteOne({_id: mongodb.ObjectId(req.body._id)})
-        .then(result => res.json(result))
+// Update the task based on the specified ID
+app.patch('/todo/:id', ensureAuthenticated, (req, res) => {
+    collection.findOne({_id: mongodb.ObjectId(req.params.id)}).then(result => {
+        // If a different user tries to delete a task, return 403 Forbidden
+        if (req.user.id !== result.github_id) {
+            res.status(403).send()
+        } else {
+            collection
+                .updateOne({_id: mongodb.ObjectId(req.params.id)}, {
+                    $set: {
+                        task: req.body.task,
+                        due_date: req.body.due_date,
+                        priority: determinePriority(req.body.due_date)
+                    }
+                }).then(result => res.json(result))
+        }
+    })
 })
 
-app.patch('/update', ensureAuthenticated, (req, res) => { // TODO update path
-    collection
-        .updateOne({_id: mongodb.ObjectId(req.body._id)}, {$set: {name: req.body.name}})
-        .then(result => res.json(result))
+// Delete the task based on the specified ID
+app.delete('/todo/:id', ensureAuthenticated, (req, res) => {
+    collection.findOne({_id: mongodb.ObjectId(req.params.id)}).then(result => {
+        // If a different user tries to delete a task, return 403 Forbidden
+        if (req.user.id !== result.github_id) {
+            res.status(403).send()
+        } else {
+            collection
+                .deleteOne({_id: mongodb.ObjectId(req.params.id)})
+                .then(result => res.json(result))
+        }
+    })
 })
+
+// HELPER FUNCTIONS
+
+const determinePriority = function (due_date) {
+    const seconds_in_day = 86400
+    const end = new Date(due_date)
+
+    const now = Date.now();
+    const timeDifference = (end - now) / 1000;
+
+    console.log('Time Difference:', timeDifference)
+
+    if (end < now) // If already past due date, then mark as late
+        return 'Late'
+    else if (timeDifference < seconds_in_day * 3) // Less than 3 days
+        return 'High'
+    else if (timeDifference < seconds_in_day * 7) // Less than 7 days
+        return 'Medium'
+    else
+        return 'Low'
+}
+
+function logger(req, res, next) {
+    console.log(req.method, req.path)
+    return next()
+}
+
+function connectionChecker(req, res, next) {
+    if (collection !== undefined) {
+        return next()
+    } else res.status(503).send()
+}
+
+// Allow the user to continue if they are going to an allowed page OR if they are authenticated
+const allowed = ['/login', '/auth/github', '/auth/github/callback']
+
+function ensureAuthenticated(req, res, next) {
+    if (allowed.includes(req.path) || req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login')
+}
 
 app.listen(process.env.PORT || 3000)
