a20663edb74fac157f9fe109e454fccfe3dd8c91
Alex Bolduc <aebolduc@wpi.edu>
2022-09-13 14:29:14 -0400

    Store session data in mongodb

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ require('dotenv').config();
 const express = require('express'), app = express();
 const session = require('express-session');
 const mongoose = require("mongoose");
+const MongoStore = require('connect-mongo');
 const passport = require('passport');
 
-const {Game, User} = require("./models");
 const {router: authRouter, requireAuth} = require("./routes/auth");
 const gamesRouter = require("./routes/games");
 
-mongoose.connect(`mongodb+srv://axbolduc:${process.env.MONGO_DB_PASS}@cluster0.3vkednf.mongodb.net/bb-stats?retryWrites=true&w=majority`).then(
+mongoose.connect(process.env.MONGO_DB_URL).then(
     () => {console.log("Connected to MongoDB")},
 )
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl: process.env.MONGO_DB_URL})
     }));
 app.use(passport.initialize());
 app.use(passport.session());
