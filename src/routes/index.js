452b7a6cafe1e39b2edca81cb90a47479ca16d19
dtavana <datavana@gmail.com>
2022-09-14 15:08:49 -0400

    Serve HTML files on correct routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
+const path = require("path");
 const bodyParser = require("body-parser");
 
+router.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/html/index.html"))
+);
+router.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/html/login.html"))
+);
 router.use("/api", bodyParser.json(), require("./api"));
 
 module.exports = router;
