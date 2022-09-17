c30fa29c72f4fe080508672fcf2f1640ededa17e
Livingwell088 <58043224+Livingwell088@users.noreply.github.com>
2022-09-17 10:14:41 -0400

    Initial index.html, server, and mongo connection

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express"),
+    app = express(),
+    logs = []
+// const path = require("path")
+
+app.use(express.static("public"))
+app.use(express.static("views"))
+app.use(express.json())
+
+app.post( '/submit', (req, res) => {
+    logs.push(req.body.newLog)
+    res.write(200, {"Content-Type": "application/json"})
+    res.end(JSON.stringify(logs))
+})
+
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:admin@a3.edjsrmm.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect()
+    .then( () => {
+        // will only create collection if it doesn't exist
+        return client.db( 'datatest5' ).collection( 'test' )
+    })
+    .then( collection => {
+        // blank query returns all documents
+        return collection.find({ }).toArray()
+    })
+    .then( console.log )
+
+
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
+
+
+
+
+app.listen( process.env.PORT || 3000 )
