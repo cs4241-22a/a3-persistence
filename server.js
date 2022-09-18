8f57a69442c48f59bc2925d9d4d697dd8b2dba96
Caleb Talley <86822049+CalebTalley2024@users.noreply.github.com>
2022-09-18 18:50:26 -0400

    Added MongoDB and mre
    - added html from a2
    - added env variables
    - made sure that MongoDB was able to connect to the database
    - downloaded dotenv
    -

--- server.js
+++ server.js
@@ -1,40 +1,44 @@
 //     http://localhost:3000/ 
 const express = require( 'express' ),
+      mongodb = require('mongodb')
       app = express()
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+require("dotenv").config();
+
+
+console.log(process.env.HOST);
 app.use( express.static( 'public' ) )
 app.use( express.static( 'views'  ) ) 
 
 
-const logger = (req,res,next) => {
-  console.log( 'url:', req.url )
-  next()
-  console.log ("hello")
-}
-app.use( logger )
+// const logger = (req,res,next) => {
+//   console.log( 'url:', req.url )
+//   next()
+  
+// }
+// app.use( logger )
 
-app.get( '/', ( req, res ) => res.send( 'Hello World!' ) )
+// app.get( '/', ( req, res ) => res.send( 'Hello World!' ) )
 
-// const port = 3000
-// app.listen(port, () =>console.info(`Listening on port ${port}`))
+
+
+app.use( express.json() )
+
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}`
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect()
+  .then( () => {
+    // will only create collection if it doesn't exist
+    return client.db( 'datatest5' ).collection( 'test' )
+  })
+  .then( collection => {
+    // blank query returns all documents
+    return collection.find({ }).toArray()
+  })
+  .then( console.log )
+
 app.listen( process.env.PORT || 3000 )
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// //const uri = "mongodb+srv://tester:tester123@cluster0.f9zkh.mongodb.net/?retryWrites=true&w=majority";
-// const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}`
-
-// //const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology:true })
-
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect()
-//   .then( () => {
-//     // will only create collection if it doesn't exist
-//     return client.db( 'datatest5' ).collection( 'test' )
-//   })
-//   .then( collection => {
-//     // blank query returns all documents
-//     return collection.find({ }).toArray()
-//   })
-//   .then( console.log )
-
