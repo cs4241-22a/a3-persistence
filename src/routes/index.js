147325423de293e35dc91e959c993baa92ce55b3
dtavana <datavana@gmail.com>
2022-09-19 16:02:37 -0400

    Implement morgan package

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const path = require("path");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 router.get("/", require("../middleware/is-logged-in"), (req, res) =>
   res.sendFile(path.join(__dirname, "../public/index.html"))
@@ -8,7 +9,7 @@ router.get("/", require("../middleware/is-logged-in"), (req, res) =>
 router.get("/login", require("../middleware/is-not-logged-in"), (req, res) =>
   res.redirect("/auth/github")
 );
-router.use("/api", bodyParser.json(), require("./api"));
+router.use("/api", bodyParser.json(), morgan("tiny"), require("./api"));
 router.use("/auth", require("./auth"));
 
 module.exports = router;
