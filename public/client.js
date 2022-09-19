e27b2fef47a66c954a03a9a6c0de394ad87095db
Caleb Talley <86822049+CalebTalley2024@users.noreply.github.com>
2022-09-19 16:27:01 -0400

    starting client code
    currently have errors with fetch ( error 405) and haivng access to public and view folder when using port 3000 ( work fine when using live server)

--- public/client.js
+++ public/client.js
@@ -1,24 +1,48 @@
-const submit = function( e ) {
+  window.onload = function() {
+    
+    const button = document.querySelector( '#addButton' )
+    button.onclick = add
+
+    const button2 = document.querySelector('#removeButton')
+    button2.onclick = remove
+
+    // const button3 = document.querySelector('EditButton')
+    // button2.onclick = edit
+  }
+
+    const remove = function(){ console.log("Hello Again")}
+const add = function( e ) {
+
     // prevent default form action from being carried out
     e.preventDefault()
 
-    const input = document.querySelector( '#yourname' ),
-          json = { yourname: input.value },
-          body = JSON.stringify( json )
+    const TaskBase = document.querySelector("#TaskBase")
+    // clears values on client side each time submit is pressed
+    TaskBase.innerText = " "
 
-    fetch( '/submit', {
+    const input = document.querySelector( '#newTask' ) 
+    const field1 = document.querySelector( '#TypeOfTask')
+    const field2 = document.querySelector('#Difficulty')
+    const field3= document.querySelector("#Semester")
+debugger
+      json  = { Task: input.value, TypeOfTask: field1.value, Difficulty: field2.value, Semester: field3.value},
+      body = JSON.stringify( json )
+    
+    fetch( '/add', {
       method:'POST',
       body 
     })
     .then( function( response ) {
       // do something with the reponse 
       console.log( response )
+      // return response.json()
+      json.forEach( item =>{
+        //////////MAKE initial elements in a div clear before adding new data
+        let tr = document.createElement('tr')
+        tr.innerText = JSON.stringify(item)
+        TaskBase.appendChild(tr)
+      })
     })
 
     return false
-  }
-
-  window.onload = function() {
-    const button = document.querySelector( 'button' )
-    button.onclick = submit
   }
--- server.js
+++ server.js
@@ -1,31 +1,25 @@
-//     http://localhost:3000/ 
+//  http://localhost:3000/ 
+// you can use npm start to also start the server
 const express = require( 'express' ),
       mongodb = require('mongodb')
       app = express()
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
 require("dotenv").config();
-
 
 console.log(process.env.HOST);
 app.use( express.static( 'public' ) )
-app.use( express.static( 'views'  ) ) 
-
-
-// const logger = (req,res,next) => {
-//   console.log( 'url:', req.url )
-//   next()
-  
-// }
-// app.use( logger )
-
-// app.get( '/', ( req, res ) => res.send( 'Hello World!' ) )
-
-
-
+app.use( express.static( 'views'  ) )
 app.use( express.json() )
 
+// logger function
+  // const logger = (req,res,next) => {
+  //   console.log( 'url:', req.url )
+  //   next()
+    
+  // }
+  // app.use( logger )
+
+  // app.get( '/', ( req, res ) => res.send( 'Hello World!' ) )
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}`
 
@@ -41,4 +35,18 @@ client.connect()
   })
   .then( console.log )
 
+  // route to get all docs for MongoDB
+app.get( '/', (req,res) => {
+  if( collection !== null ) {
+    // get array and pass to res.json
+    collection.find({ }).toArray().then( result => res.json( result ) )
+  }
+})
+
+app.post( '/add', (req,res) => {
+  // assumes only one object to insert
+  collection.insertOne( req.body ).then( result => res.json( result ) )
+})
 app.listen( process.env.PORT || 3000 )
+
+
