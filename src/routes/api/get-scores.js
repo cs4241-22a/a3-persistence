7aa8842b1c4dd25006d628d7ec5eb55fa3af7dee
dtavana <datavana@gmail.com>
2022-09-19 15:43:40 -0400

    Remove unneeded log statement

--- src/routes/api/get-scores.js
+++ src/routes/api/get-scores.js
@@ -1,7 +1,6 @@
 const ScoreEntry = require("../../models/ScoreEntry");
 
 const getScores = async (req, res, next) => {
-  console.log(req.user);
   const { username: githubUsername } = req.user;
   const allScores = await ScoreEntry.find({ githubUsername });
   res.send(allScores);
