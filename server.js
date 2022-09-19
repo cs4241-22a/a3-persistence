b5059947969c79774a65d4910758667945e870d3
Grace <gyue20@gmail.com>
2022-09-19 13:24:12 -0400

    Updated server to use templating engine, made some changes to Sakura framework table styling.

--- server.js
+++ server.js
@@ -9,29 +9,62 @@ app.engine('handlebars', hbs());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.use(express.static(__dirname + "/public")); // serve up static files in the directory public
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:<password>@cluster0.e2uj8fa.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
-// app.use(function (req, res, next) { // middleware to always send unauthenticated users to the login page
-//     if (req.session && req.session.login) {
-//         console.log("user logged in!");
-//         next();
-//     } else {
-//         console.log("nope, not logged in");
-//         res.redirect('/public/index.html');
-//         return;
-//     }
-// });
+app.use(express.static(__dirname + "/public")); // serve up static files in the directory public
 
 app.use(cookie({ // cookie middleware
     name: 'session',
     keys: ['key1', 'key2'] // TODO: secure these
 }));
 
+let appdata = [
+    {
+        show: "Game of Thrones",
+        seasons: 8,
+        episodes: 10,
+        duration: 60,
+        totalTime: 80.0,
+    },
+    {
+        show: "House of the Dragon",
+        seasons: 1,
+        episodes: 10,
+        duration: 60,
+        totalTime: 10.0,
+    },
+    {
+        show: "Better Call Saul",
+        seasons: 6,
+        episodes: 10,
+        duration: 45,
+        totalTime: 45.0,
+    },
+];
+
+// add some middleware that always sends unauthenticated users to the login page
+// app.use(function (req, res, next) {
+//     console.log("I am now checking to see if the user is logged in!");
+//     if (req.session.login) {
+//         console.log("You are logged in!");
+//         next();
+//     } else {
+//         console.log("not logged in...");
+//         res.render('index', { msg: 'You are not logged in', layout: false });
+//     }
+// });
+
 app.get('/account', function (req, res) {
     if (req.session.login) {
-        res.send('Welcome back, ' + req.session.username + '!');
-        //res.render( 'main', { msg:'success you have logged in', layout:false })
-        res.end();
+        console.log("Logged in");
+        res.render('main', { msg: 'success you have logged in', layout: false });
     } else {
         res.send('Please login to view this page!');
         res.end();
@@ -41,16 +74,40 @@ app.get('/account', function (req, res) {
 
 // manage login form requests
 app.post('/register', express.json(), (req, res) => {
+    console.log(req.body);
     if (req.body.password === "test") {
-        console.log("Correct password");
-        req.session.username = req.body.username;
         req.session.login = true;
-        console.log(req.session);
-        res.redirect('/public/account.html');
+        req.session.username = req.body.username;
+        console.log("Rendering main view");
+        res.render('main', { msg: 'login successful!' });
     } else {
         req.session.login = false;
-        console.log("Redirecting to index.html");
-        res.redirect('/public/index.html');
+        console.log("Rendering failed view");
+        res.render('index', { msg: 'login failed, please try again', layout: false });
+    }
+});
+
+app.post('/submit', express.json(), (req, res) => {
+    console.log("Something got submitted to the TV show form");
+});
+
+app.post('/remove', express.json(), (req, res) => {
+    console.log("Attempting to remove something");
+    res.writeHead(200, "OK", {
+        "Content-Type": "text/plain",
+    });
+    res.end(JSON.stringify(appdata));
+});
+
+app.post('/logout', express.json(), (req, res, next) => {
+    console.log("Logging out");
+    if (req.session.login) {
+        req.session.username = "";
+        req.session.login = false;
+        console.log("Re-rendering");
+        res.render('main', { msg: 'Logged out successfully!', layout: false });
+    } else {
+        next();
     }
 });
 
