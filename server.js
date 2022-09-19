1ebdc87921d2ba12b1fbfdceb6acf3210cce467f
Livingwell088 <58043224+Livingwell088@users.noreply.github.com>
2022-09-19 09:46:31 -0400

    Mongo Insert complete

--- server.js
+++ server.js
@@ -12,28 +12,45 @@ app.use(express.json())
 app.post( '/submit', (req, res) => {
     console.log(req.body);
     logs.push(req.body)
-    res.writeHead(200, {"Content-Type": "application/json"})
-
-    res.end(JSON.stringify(logs))
+    collection.insertOne(req.body).then(result => res.json(result))
+    // res.writeHead(200, {"Content-Type": "application/json"})
+    //
+    // res.end(JSON.stringify(logs))
 })
 
+// app.get('/', (req, res) => {
+//     const cursor = db.collection('Activity Logs').find()
+//
+// })
 
 
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = 'mongodb+srv://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST + ".edjsrmm.mongodb.net/?retryWrites=true&w=majority";
+
 const uri = "mongodb+srv://admin:admin@a3.edjsrmm.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+let collection = null
+
 client.connect()
     .then( () => {
         // will only create collection if it doesn't exist
-        return client.db( 'datatest5' ).collection( 'test' )
+        return client.db( 'A3' ).collection( 'Activity Logs' )
     })
-    .then( collection => {
+    .then( __collection => {
+        collection = __collection;
         // blank query returns all documents
         return collection.find({ }).toArray()
     })
     .then( console.log )
+
+app.get( '/', (req, res) => {
+    if (collection !== null){
+        collection.find({ }).toArray().then(result => res.json(result))
+    }
+})
 
 
 // client.connect(err => {
