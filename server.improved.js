f4b1f7cb2ad54b4bbf8a42c8ce186d5514f7ec3d
Glitch (a2-othniel9) <none>
2022-09-20 18:23:15 +0000

    🍴🛕 Updated with Glitch

--- server.improved.js
+++ server.improved.js
@@ -2,12 +2,12 @@ const express = require('express'),
       app = express()
 
 //creat[ing onew middleware]
-// app.use( (req,res,next) => {
-//   console.log( req.url )
-//   next()
-// })
+app.use( (req,res,next) => {
+  console.log( req.url )
+  next()
+})
 
-app.use(express.static('./'))
+app.use(express.static("public"))
 
 // app.get( '/', ( req, res ) => res.send( 'test') )
 
