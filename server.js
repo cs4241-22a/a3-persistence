0eacd519fa9764aaee63ac376148e7bfcf545cbe
Abby Hyde <abbyghyde@gmail.com>
2022-09-20 18:53:35 -0400

    added mongodb back in doesn't work yet, fixed styling

--- server.js
+++ server.js
@@ -21,6 +21,32 @@ app.use( cookie({
 // serve up static files in the directory views
 app.use( express.static('public') )
 app.use( express.static('views') )
+app.use( express.json() )
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+//const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}%40${process.env.HOST}`
+const uri = `mongodb+srv://abbyghyde:QJB8YySPahsy794G@cluster0-a3.fy7bqls.mongodb.net`
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+let collection = null
+
+// // const connect = async function() {
+// //   await client.connect()
+// //   const collection = await client.db("groceries").collection("devices")
+// //   const results = await collection.find({}).toArray()
+// //   console.log(results)
+// // }
+
+client.connect()
+  .then( () => {
+    // will only create collection if it doesn't exist
+    return client.db( 'groceries' ).collection( 'devices' )
+  })
+  .then( collection => {
+    // blank query returns all documents
+    return collection.find({ }).toArray()
+  })
+  .then( console.log )
 
 app.post( '/login', (req,res)=> {
   // express.urlencoded will put your key value pairs 
@@ -52,6 +78,11 @@ app.post( '/login', (req,res)=> {
 
 app.get( '/', (req,res) => {
   res.render( 'index', { msg:'', layout:false })
+  // db stuff
+  if( collection !== null ) {
+    // get array and pass to res.json
+    collection.find({ }).toArray().then( result => res.json( result ) )
+  }
 })
 
 // add some middleware that always sends unauthenicaetd users to the login page
@@ -66,75 +97,33 @@ app.get( '/main.html', ( req, res) => {
   res.render( 'main', { msg:'success you have logged in', layout:false })
 })
 
+app.use( (req,res,next) => {
+  if( collection !== null ) {
+    next()
+  }else{
+    res.status( 503 ).send()
+  }
+})
+
+app.post( '/add', (req,res) => {
+  // assumes only one object to insert
+  collection.insertOne( req.body ).then( result => res.json( result ) )
+})
+
+// assumes req.body takes form { _id:5d91fb30f3f81b282d7be0dd } etc.
+app.post( '/remove', (req,res) => {
+  collection
+    .deleteOne({ _id:mongodb.ObjectId( req.body._id ) })
+    .then( result => res.json( result ) )
+})
+
+app.post( '/update', (req,res) => {
+  collection
+    .updateOne(
+      { _id:mongodb.ObjectId( req.body._id ) },
+      { $set:{ name:req.body.name } }
+    )
+    .then( result => res.json( result ) )
+})
+
 app.listen( 3000 )
-
-// // const express = require( 'express' ),
-// //       mongodb = require( 'mongodb' ),
-// //       app = express()
-
-// // app.use( express.static('public') )
-// // app.use( express.json() )
-
-// // const { MongoClient, ServerApiVersion } = require('mongodb');
-// // //const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}%40${process.env.HOST}`
-// // const uri = `mongodb+srv://abbyghyde:QJB8YySPahsy794G@cluster0-a3.fy7bqls.mongodb.net`
-
-// // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// // let collection = null
-
-// // // const connect = async function() {
-// // //   await client.connect()
-// // //   const collection = await client.db("groceries").collection("devices")
-// // //   const results = await collection.find({}).toArray()
-// // //   console.log(results)
-// // // }
-
-// // client.connect()
-// //   .then( () => {
-// //     // will only create collection if it doesn't exist
-// //     return client.db( 'groceries' ).collection( 'devices' )
-// //   })
-// //   .then( collection => {
-// //     // blank query returns all documents
-// //     return collection.find({ }).toArray()
-// //   })
-// //   .then( console.log )
-  
-// // // route to get all docs
-// // app.get( '/', (req,res) => {
-// //   if( collection !== null ) {
-// //     // get array and pass to res.json
-// //     collection.find({ }).toArray().then( result => res.json( result ) )
-// //   }
-// // })
-
-// // // app.use( (req,res,next) => {
-// // //   if( collection !== null ) {
-// // //     next()
-// // //   }else{
-// // //     res.status( 503 ).send()
-// // //   }
-// // // })
-
-// // // app.post( '/add', (req,res) => {
-// // //   // assumes only one object to insert
-// // //   collection.insertOne( req.body ).then( result => res.json( result ) )
-// // // })
-
-// // // // assumes req.body takes form { _id:5d91fb30f3f81b282d7be0dd } etc.
-// // // app.post( '/remove', (req,res) => {
-// // //   collection
-// // //     .deleteOne({ _id:mongodb.ObjectId( req.body._id ) })
-// // //     .then( result => res.json( result ) )
-// // // })
-
-// // // app.post( '/update', (req,res) => {
-// // //   collection
-// // //     .updateOne(
-// // //       { _id:mongodb.ObjectId( req.body._id ) },
-// // //       { $set:{ name:req.body.name } }
-// // //     )
-// // //     .then( result => res.json( result ) )
-// // // })
-
-// // app.listen( 3000 )
