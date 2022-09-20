08049c29d35b0c055b4293a8a7c199d7f1744160
rmbrunelle <rmbrunelle2@wpi.edu>
2022-09-20 15:46:50 -0400

    working version of a2 using express now

---
+++ scripts.js
@@ -0,0 +1,154 @@
+console.log('in scripts')
+const songs = []
+const songList = document.getElementById('results').getElementsByTagName('tbody')[0]
+const songForm = document.forms[0]
+const songInput = songForm.elements['songname']
+const artistInput = songForm.elements['artist']
+const durationInput = songForm.elements['songduration']
+const albumInput = songForm.elements['album']
+
+
+const addToPlaylist = function(songData) 
+{  
+    const newRow = songList.insertRow()
+    const newCell = newRow.insertCell()
+    const artistCell = newRow.insertCell()
+    const durationCell = newRow.insertCell()
+    const albumCell = newRow.insertCell()
+    const playlistDurationCell = newRow.insertCell()
+    
+    const newSong = document.createTextNode(songData[0])
+    const newArtist = document.createTextNode(songData[1])
+    const newDuration = document.createTextNode(songData[2])
+    const newAlbum = document.createTextNode(songData[3])
+    const newPlaylistDuration = document.createTextNode(songData[4])
+    //newSong.innerHTML = song
+    newCell.appendChild(newSong)
+    artistCell.appendChild(newArtist)
+    durationCell.appendChild(newDuration)
+    albumCell.appendChild(newAlbum)
+    playlistDurationCell.appendChild(newPlaylistDuration)
+  
+   // const table = document.getElementById("results")
+   // table.innerHTML = "<tr id='firstRow'><th>Song</th><th>Artist</th><th>Duration</th><th>Album</th><th>Playlist length</th></tr>"
+
+  //  newData.forEach((element, index) => {
+
+        // if(element.songname === undefined)
+        // {
+        //     ;
+        // }
+        // else
+        // {
+        // test  table.innerHTML += "<tr><td>" + song + "</td></tr>"
+        // table.innerHTML +=
+        //   "<tr><td>" + element.songname + "</td><td>"
+        //   + element.artist + "</td><td>"
+        //   + element.duration + "</td><td>"
+        //   + element.album + "</td><td class='tally'>"
+        //   + element.playlistDur + "</td></tr>"
+        //}
+   //   })
+  }
+
+
+songs.forEach( function(song) {
+  addToPlaylist(song)
+})
+
+songForm.onsubmit = function(e)
+{
+    e.preventDefault()
+
+    console.log("in script submit function")
+    // let songname = document.querySelector("#songname")
+    // let artist = document.querySelector("#artist")
+    // let duration = document.querySelector("#songduration")
+    // let album = document.querySelector("#album")
+
+    const songValue = songInput.value
+    const artistValue = artistInput.value
+    const durationValue = durationInput.value
+    const albumValue = albumInput.value
+    
+    let previousDuration = 0
+    if(songs.length>0)
+      {
+        previousDuration += parseInt(songs[songs.length-1][4])
+      }
+  
+    const playlistDuration = previousDuration + parseInt(durationInput.value)
+    
+    const songData = [songValue, artistValue, durationValue, albumValue, playlistDuration]
+    
+    songs.push(songData)
+    addToPlaylist(songData)
+  
+    //reset form
+    songInput.value = ''
+    songInput.focus()
+    artistInput.value = ''
+    artistInput.focus()
+    durationInput.value = ''
+    durationInput.focus()
+    albumInput.value = ''
+    albumInput.focus()
+  
+    // let json = {
+    //   songname: songname.value,
+    //   artist: artist.value,
+    //   duration: duration.value,
+    //   album: album.value,
+    //   playlistDur: 0
+    // }
+    // let body = JSON.stringify(json)
+
+    fetch( '/submit', {
+      method:'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body:JSON.stringify({ "newsong":songData})
+    })
+    .then( res => res.json() )
+    .then( json => console.log( json ) )
+    // return false
+}
+
+
+
+
+
+//   const reset = function(e)
+//   {
+//       e.preventDefault()
+//       body = JSON.stringify("")
+
+//       fetch( '/reset', {
+//         method:'POST',
+//         body
+//       })
+//       .then(async function(response)
+//       {
+//         let emptyData = await response.json()
+//         emptyPlaylist(emptyData)
+//         console.log(emptyData)
+//       })
+//       return false
+      
+//   }
+
+  // function emptyPlaylist(newData)
+  // {
+  //   const table = document.getElementById("results")
+  //   table.innerHTML = "<tr id='firstRow'><th>Song</th><th>Artist</th><th>Duration</th><th>Album</th><th>Playlist length</th></tr>"
+  // }
+
+
+//   window.onload = function() 
+//   {
+//     const submitButton = document.getElementById( 'submitButton' )
+//     submitButton.onclick = submit
+
+//     const resetButton = document.getElementById( 'resetButton' )
+//     resetButton.onclick = reset
+//   }
+
--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const express = require('express'),
     app = express()
 
-//app.get('/', function(req, res) {res.end('test')})
+const bodyparser = require('body-parser')
 
-//app.get('/index.html', function(req, res) {res.end('test')})
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const db = low( new FileSync( 'data.json'))
 
+db.defaults({ songs:[] }).write()
+app.use(express.static('./'))
 
+console.log('in server')
 
-
-
-
-// const logger = (req, res, next)=> {
-//     console.log(req.url)
-//     next()
-// }
-
-// app.use( logger )
+// app.use(bodyParser.json())
 
 // app.use(function(req, res, next)
 // {
@@ -23,23 +20,34 @@ const express = require('express'),
 //     next()
 // })
 
-// app.use(express.static('./'))
+// app.use(bodyParser.urlencoded({
+//     extended: false
+// }))
 
-// app.get('/', function(req, res) {res.send('Hello world!')})
+// app.use(express.urlencoded({
+//     extended: true
+// }))
 
 
-// app.listen(3000)
+app.get('/', function(req, res){res.sendFile("index.html")})
 
+app.post( '/submit', bodyparser.json(), function( req, res )  {
+    console.log('recieving data...')
+    // const testSongName = req.body.testSongName
+    // const songname = req.body.songname
+    // const artistname = req.body.artist
+    // const songduration = req.body.songduration
+    // const albumname = req.body.album
+    // console.log(testSongName)
+    // res.send(testSongName)
 
+    //res._write(testSongName)
+    db.get( 'songs' ).push(req.body.newsong).write()
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify(db.getState()))
+})
 
-app.use(express.urlencoded({
-    extended: true
-}))
-
-app.post('server.js', (req, res) => {
-    const songname = req.body.songname
-    const artistname = req.body.artist
-    const songduration = req.body.songduration
-    const albumname = req.body.album
-    res.end()
+//app.listen(3000)
+const listener = app.listen( process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port )
 })
