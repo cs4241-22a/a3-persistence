7bf128eba3f6ba23e77a232214f2cc17be244959
Dooober <cooperdean2002@gmail.com>
2022-09-20 17:50:04 -0400

    Login?
    I couldn't test on laptop

--- server.js
+++ server.js
@@ -1,17 +1,52 @@
 const express = require('express')
+const cookie = require('cookie-session')
 const app = express()
-const port = 3000
 
 app.use(express.static('public'))
+app.use( express.urlencoded({ extended:true }) )
+
+app.use( cookie({
+    name: 'session',
+    keys: ['key1', 'key2']
+  }))
 
 app.get('/', (req, res) => {
     res.send('/index.html')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.post( '/login', (req,res)=> {
+    // express.urlencoded will put your key value pairs 
+    // into an object, where the key is the name of each
+    // form field and the value is whatever the user entered
+    console.log( req.body )
+    
+    // below is *just a simple authentication example* 
+    // for A3, you should check username / password combos in your database
+    if( req.body.password === 'test' ) {
+      // define a variable that we can check in other middleware
+      // the session object is added to our requests by the cookie-session middleware
+      req.session.login = true
+      
+      // since login was successful, send the user to the main content
+      // use redirect to avoid authentication problems when refreshing
+      // the page or using the back button, for details see:
+      // https://stackoverflow.com/questions/10827242/understanding-the-post-redirect-get-pattern 
+      res.redirect( 'main.html' )
+    }else{
+      // password incorrect, redirect back to login page
+      res.sendFile( __dirname + '/public/index.html' )
+    }
+  })
+  
+  // add some middleware that always sends unauthenicaetd users to the login page
+  app.use( function( req,res,next) {
+    if( req.session.login === true )
+      next()
+    else
+      res.sendFile( __dirname + '/public/index.html' )
+  })
 
+app.listen( process.env.PORT || 3000 )
 
 const weaknesses = {
     'Normal': ['Fighting'],
