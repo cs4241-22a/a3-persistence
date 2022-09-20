60c0e292f511092db51c7ece3c1482b6af13ea85
2bias <tmack@wpi.edu>
2022-09-20 11:07:44 -0400

    mongoDb connection

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const express = require("express"),
-    app = express()
+const express = require("express")
+const app = express()
+const mongodb = require('mongodb')
+require('dotenv').config()
+
 
 app.use(express.static('public'))
 app.use(express.static('views'))
@@ -8,4 +11,40 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+//MONGO DB
+const uri = 'mongodb+srv://'+process.env.ADMIN+':'+process.env.PASS+'@'+process.env.HOST
+const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology:true })
+let collection = null
+
+client.connect()
+  .then( () => {
+    // will only create collection if it doesn't exist
+    return client.db( 'XXXtest' ).collection( 'XXXtodos' )
+  })
+  .then( __collection => {
+    // store reference to collection
+    collection = __collection
+    // blank query returns all documents
+    return collection.find({ }).toArray()
+  })
+  .then( console.log )
+  
+// route to get all docs
+app.get( '/', (req,res) => {
+  if( collection !== null ) {
+    // get array and pass to res.json
+    collection.find({ }).toArray().then( result => res.json( result ) )
+  }
+})
+
+//check connection
+app.use( (req,res,next) => {
+    if( collection !== null ) {
+      next()
+    }else{
+      res.status( 503 ).send()
+    }
+  })
+
+//
 app.listen(3000)
