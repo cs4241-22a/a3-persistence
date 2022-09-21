a9c5f537dcc75b54ab88437d366e80a1730eb0ad
obondah <obondah@ATC8227>
2022-09-21 11:30:04 -0400

    ready for backend-server

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,45 +1,99 @@
-// Add some Javascript code here, to run on the front end.
-const Todo = []
-let task 
-let date 
-let priority 
-function main(){
-task = document.getElementById('to_do')
-date = document.querySelector('#date')
-priority = document.querySelector('#priority')
+let Todo = []
+const task = document.getElementById('to_do')
+const date = document.querySelector('#date')
+const priority = document.querySelector('#priority')
+const results = document.getElementById("results")
 const add = document.querySelector('#add')
-    add.onclick = buttonClick
+
+function isEmpty(){
+    if (task.value === "" || date.value === "" || task.value === null || date.value === null || priority.value === "none" || priority.value === null){
+        alert("Please fill out all fields")
+        return true
+    }
+    return false
 }
-function buttonClick(){
-    isEmpty()
-    submit()
+
+add.onclick = function (event){
+    event.preventDefault()
+    if (isEmpty() === false) {
+        add_row()
+    }
+}
+function add_row(){
+    const json = {
+        Task: task.value,
+        Date: date.value,
+        Priority: priority.value
+    }
+
+
+    const table_len = (results.rows.length)
+    results.insertRow(table_len).innerHTML= "<tr id='row"+table_len+"'><td id='task_row"+table_len+"'>"+task.value+"</td><td id='date_row"+table_len+"'>"+date.value+"</td><td id='priority_row"+table_len+"'>"+priority.value+"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit' class='edit' onclick='edit_row("+table_len+")'> <input hidden type='button' id='save_button"+table_len+"' value='Save' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete' class='delete' onclick='delete_row("+table_len+")'></td></tr>"
+
+    Todo.push(json)
     task.value = ""
     date.value = ""
-    priority.value = "High"
+    priority.value = ""
+    // fetch('/add', {
+    //     method: 'POST',
+    //     headers: {'Content-Type': 'application/json'},
+    //     body: JSON.stringify(json)
+    // })
+    //     .then(response => response.json())
 }
-function isEmpty(){
-    if (task.value === "" || date.value === ""){
-        alert("Please fill out all fields") 
-        return false
-    }
-    return true
+function edit_row(index)
+{
+    document.getElementById("edit_button"+index).style.display="none";
+    document.getElementById("save_button"+index).style.display="block";
+
+    let task=document.getElementById("task_row"+index);
+    let date=document.getElementById("date_row"+index);
+    let priority=document.getElementById("priority_row"+index);
+
+    let task_data=task.innerHTML;
+    let date_data=date.innerHTML;
+    let priority_data=priority.innerHTML;
+
+    task.innerHTML="<input type='text' id='task_text"+index+"' value='"+task_data+"'>";
+    date.innerHTML="<input type='text' id='date_text"+index+"' value='"+date_data+"'>";
+    priority.innerHTML="<input type='text' id='priority_text"+index+"' value='"+priority_data+"'>";
+}
+
+function save_row(index){
+
+    let task_val=document.getElementById("task_text"+index).value;
+    let date_val=document.getElementById("date_text"+index).value;
+    let priority_val=document.getElementById("priority_text"+index).value;
+
+    document.getElementById("task_row"+index).innerHTML=task_val;
+    document.getElementById("date_row"+index).innerHTML=date_val;
+    document.getElementById("priority_row"+index).innerHTML=priority_val;
+
+    document.getElementById("edit_button"+index).style.display="block";
+    document.getElementById("save_button"+index).style.display="none";
+
+ //   fetch('/edit', {
+//     method:'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body:JSON.stringify( json )
+// })
+//     .then( response => response.json())
 }
 
 
-  const submit = function( e ) {
-    // prevent default form action from being carried out
-    // e.preventDefault()
-    
-    let todo=[date.value,task.value,priority.value]
-    Todo.push(todo)
+function delete_row(){
+    //document.getElementById("row"+index).outerHTML="";
+    let i = event.target.parentNode
+    let tr = i.parentNode
+    tr.parentNode.removeChild(tr)
 
-    fetch('/', {
-    method:'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body:JSON.stringify({ "task":todo })
-  })
-     .then( response => response.json())
-     .then( json => {console.log(json)})
-  }
+    // fetch('/delete', {
+//     method:'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body:JSON.stringify( json )
+// })
+//     .then( response => response.json())
+// }
+}
 
 
--- server.improved.js
+++ server.improved.js
@@ -1,14 +1,50 @@
 const express = require('express'),
+      mongodb = require('mongodb'),
       app = express()
 
-//creat[ing onew middleware]
-app.use( (req,res,next) => {
-  console.log( req.url )
-  next()
-})
 
 app.use(express.static("public"))
-
+app.use(express.json())
 // app.get( '/', ( req, res ) => res.send( 'test') )
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://test:test@a3-persistence.nraftwq.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+let collection = null
+
+client.connect()
+    .then( () => {
+      // will only create collection if it doesn't exist
+      return client.db( 'a3-persistence' ).collection( 'table' )
+    })
+    .then( __collection => {
+      // store reference to collection
+      collection = __collection
+      // blank query returns all documents
+      return collection.find({ }).toArray()
+    })
+    .then( console.log )
+
+// route to get all docs
+app.get( '/', (req,res) => {
+  if( collection !== null ) {
+    // get array and pass to res.json
+    collection.find({ }).toArray().then( result => res.json( result ) )
+  }
+})
+
+//add
+app.post('/add',(req,res)=>{
+    collection.insertOne( req.body ).then( result => res.json( result ) )
+})
+app.post('/delete',(req,res)=>{
+
+})
+app.post('/edit',(req,res)=>{
+
+})
+
+
 
 app.listen( 3000 )
