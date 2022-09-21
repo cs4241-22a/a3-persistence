dbeb68da52ffd7d19715f227a97c6476c866bbc6
ngrumski <grumski88@gmail.com>
2022-09-20 23:12:00 -0400

    users can now check and input todos

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 require('dotenv').config()
 const express = require( 'express' ),
     cookie  = require( 'cookie-session' ),
-    engine = require('express-handlebars'),
     mongodb = require( 'mongodb' ),
     app = express()
 
 // use express.urlencoded to get data sent by defaut form actions
 // or GET requests
-app.use( express.urlencoded({ extended:true }) )
+app.use(express.urlencoded({ extended:true }) )
 
 app.use( express.json() )
 
@@ -16,9 +15,8 @@ const uri = 'mongodb+srv://'+process.env.USER+':'+process.env.PASS+'@'+process.e
 const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology:true })
 let collection = null
 let appdata = []
-let userdata = null
 
-client.connect()
+refreshDB = () => {return client.connect()
   .then( () => {
     // will only create collection if it doesn't exist
     return client.db( 'TODOs' ).collection( 'usersAndTasks' )
@@ -29,7 +27,8 @@ client.connect()
     // blank query returns all documents
     appdata = collection.find({ }).toArray()
   })
-
+}
+refreshDB()
 // cookie middleware! The keys are used for encryption and should be
 // changed
 app.use( cookie({
@@ -37,58 +36,67 @@ app.use( cookie({
   keys: ['2iMAwLWcViIKX5kAXuted14Jejr5Nwd4', '8ihbYfca2GFjh3eTvL1zpaWbgY0fZGWh']
 }))
 
-app.engine('handlebars', engine.engine());
-app.set('view engine', 'handlebars');
+app.engine('html', require('hbs').__express);
+app.set('view engine', 'hbs');
+app.set('views', './views');
 
 app.post( '/login', (req,res)=> {
-    // express.urlencoded will put your key value pairs 
-    // into an object, where the key is the name of each
-    // form field and the value is whatever the user entered
-    console.log( req.body )
-    let password = null;
-    appdata.then( (_appdata) => {
-        for(let i = 0; i < _appdata.length; i++){
-          if(req.body.username === _appdata[i].username){
-            password = _appdata[i].password
-            break
-          }
-        }
-        if( req.body.password === password ) {
-          // define a variable that we can check in other middleware
-          // the session object is added to our requests by the cookie-session middleware
-          req.session.login = true
-          res.redirect( 'main' )
-        }else{
-          // password incorrect, redirect back to login page
-          res.sendFile( __dirname + '/views/index.html' )
-          //maybe add a wrong password message
+  // express.urlencoded will put your key value pairs 
+  // into an object, where the key is the name of each
+  // form field and the value is whatever the user entered
+  appdata.then( (_appdata) => {
+      for(let i = 0; i < _appdata.length; i++){
+        if(req.body.username === _appdata[i].username){
+          req.session.user  = _appdata[i]
+          break
         }
       }
-    )
-  })
+      if(typeof req.session.user !== 'undefined' && req.body.password === req.session.user.password ) {
+        // define a variable that we can check in other middleware
+        // the session object is added to our requests by the cookie-session middleware
+        req.session.login = true
+        refreshDB()
+        .then(res.redirect( 'main' ))
+      }else{
+        // password incorrect, redirect back to login page
+        res.render("index.html", {message:"wrong password or user name"})
+      }
+    }
+  )
+})
 
 app.post( '/logout', (req,res)=> {
-      req.session.login = false
-      res.sendFile( __dirname + '/views/index.html' )
-  }) 
+  req.session.login = false
+  req.session.user = undefined
+  res.redirect( '/' )
+}) 
+
+app.post( '/checkTODO', (req, res)=>{
+  for(let i = 0; i < req.session.user.tasks.length; i++){
+    if(req.body.text === req.session.user.tasks[i].text){
+      req.session.user.tasks[i].completed = req.body.completed
+      collection.updateOne({_id:mongodb.ObjectId(req.session.user._id)}, {$set:{tasks:req.session.user.tasks}})
+      break
+    }
+  } 
+  res.status( 200 ).send()
+})
+
+app.post('/addTODO', (req, res)=>{
+  req.session.user.tasks.push({completed: false, text: req.body.text})
+  collection.updateOne({_id:mongodb.ObjectId(req.session.user._id)}, {$set:{tasks:req.session.user.tasks}})
+  res.redirect( 'main' )
+})
 
 app.get( '/main', ( req, res) => {
-    if( req.session.login === true )
-        
-        res.render(__dirname +"/views/layouts/main.handlebars",
-          {
-            task: [
-              "one task",
-              "two task",
-              "red task",
-              "blue task"
-            ]
-          } 
-        )
-    else{
-        res.sendFile( __dirname + '/views/index.html' )
+    if( req.session.login === true ){
+      res.render("main.html", req.session.user)
+    }else {
+      res.redirect( '/' )
     }
 })
+
+app.get('/', (req, res)=>{res.render("index.html", {message:""})})
 
 app.use( (req,res,next) => {
   if( collection !== null ) {
@@ -97,8 +105,5 @@ app.use( (req,res,next) => {
     res.status( 503 ).send()
   }
 })
-
-app.use( express.static( 'public' ) )
-app.use( express.static( 'views'  ) ) 
 
 app.listen( process.env.PORT || 3000 )
