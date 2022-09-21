56ff4cc1408915fc23c28fa688a7e0174259327d
Charlie Snow <cesnow@wpi.edu>
2022-09-21 17:04:27 -0400

    Update server.improved.js

--- server.improved.js
+++ server.improved.js
@@ -2,13 +2,13 @@ const express = require( 'express' ),
       mongodb = require( 'mongodb' ),
       app = express()
 
-app.get('/', (req, res) => res.send('Hello Wordld!'))
+//app.get('/', (req, res) => res.send('Hello Wordld!'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 app.use( express.static('public') )
 app.use( express.json() )
 
-const uri = 'mongodb+srv://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST
+const uri = 'mongodb+srv://csnow99:0w9PUDhNCjhPwJsk@cluster0.vwmaw2f.mongodb.net/test'
 
 const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology:true })
 let collection = null
@@ -21,10 +21,14 @@ client.connect()
   })
   .then( console.log )
   
-// route to get all docs
+/*// route to get all docs
 app.get( '/', (req,res) => {
   if( collection !== null ) {
     // get array and pass to res.json
     collection.find({ }).toArray().then( result => res.json( result ) )
   }
-})
+})*/
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/public/index.html");
+});
