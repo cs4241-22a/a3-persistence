bc9c882ea919ea4eae596ae6b32786d4cf828009
hyoder <hryoder@wpi.edu>
2022-09-21 20:58:12 -0400

    init commit

---
+++ public/js/scripts.js
@@ -0,0 +1,87 @@
+let tempID = null, tempElement = null;
+function dothething()
+{
+    fetch('/data', { method: 'GET', headers: { "Content-Type": "application/json" } })
+    .then( response => response.json() )
+    .then( json => builder( json[ 0 ].entries ));
+};
+function builder ( json )
+{
+    const tbl = document.getElementById( 'datatable' );
+    tbl.innerHTML = '<tr><th>title</th><th>genre</th><th>year</th><th>directed by Todd Phillips?</th><th>edit?</th><th>delete?</th></tr>';
+    json.forEach(entry => 
+    {
+        let newRow  = '<tr id = "' + entry.id + '">';
+            newRow += '<th>' + entry.title.toString().toUpperCase() + '</th>';
+            newRow += '<th>' + entry.genre.toString().toUpperCase() + '</th>';
+            newRow += '<th>' + entry.year + '</th>';
+            newRow += '<th>' + toddcheck( entry.title, entry.year ) + '</th>';
+            newRow += '<th><button onclick="updater(' + entry.id + ',' + entry.title + ',' + entry.genre + ',' + entry.year + ');">submit</button></th>';
+            newRow += '<th><button onclick="deleter(' + entry.id + ');">delete?</button></th>';
+        datatable.innerHTML += newRow;
+    });
+};
+function submitter( e )
+{
+    const title = document.getElementById('title'), 
+          genre = document.getElementById('genre'), 
+           year = document.getElementById('year'), 
+           json = { title: title.value, genre: genre.value, year: year.value }, 
+           body = JSON.stringify(json);
+    fetch('/submit', { method: 'POST', headers: { "Content-Type": "application/json" }, body })
+    .then( response => response.json())
+    .then(dothething());
+    return false;
+};
+function deleter( id )
+{
+    const json = { id: x.id }, 
+          body = JSON.stringify( json );
+    fetch( '/delete', { method: 'POST', headers: { "Content-Type": "application/json" }, body } )
+    .then( response => { response.json() })
+    .then( dothething() );
+    return false;
+};
+function updater( id, title, genre, year )
+{
+    if( tempElement != null && tempID != null ) { updatercanceller( tempID ); }
+    tempID = id;
+    tempElement = document.getElementById('row-'+id);
+    let newRow  = '<th>' + id + '</th>';
+        newRow += '<th><input type="text"   value="' + title + '"></th>'
+        newRow += '<th><input type="text"   value="' + genre + '"></th>'
+        newRow += '<th><input type="number" value="' +  year + '"></th>'
+        newRow += '<th><button onclick="updatercanceller(' + id + ')>cancel?</button></th>';
+        newRow += '<th><button onclick="updatersubmiter(' + id + ')>submit</button></th>';
+    tempElement.innerHTML = newRow;
+};
+function updatercanceller( id )
+{
+    let title  = document.getElementById( 'title-${tempID}' ).value,
+        genre  = document.getElementById( 'genre-${tempID}' ).value,
+         year  = document.getElementById(  'year-${tempID}' ).value,
+       newRow  = '<th>' + tempID + '</th>';
+       newRow += '<th>' + title + '</th>';
+       newRow += '<th>' + genre + '</th>';
+       newRow += '<th>' + year + '</th>';
+       newRow += '<th>' + toddCheck(title, genre) + '<\th>';
+       newRow += '<th><button onclick="updater(\'${entry.id}\', \'${entry.title}\', \'${entry.genre}\', \'${entry.year}\');">edit?</button></th>';
+       newRow += '<th><button onclick="deleter(\'${entry.id}\');"">delete?</button></th>';
+    tempElement.innerHTML = newRow;
+    tempElement = null;
+    tempID = null;
+};
+function updatersubmitter( id )
+{
+    const json = {    id: id,
+                   title: document.getElementById('title-'+id).value,
+                   genre: document.getElementById('genre-'+id).value,
+                    year: document.getElementById( 'year-'+id).value
+                 },
+          body = JSON.stringify(json);
+    fetch( '/update', { method: 'POST', headers: { "Content-Type": "application/json" }, body })
+    .then( response => response.json() )
+    .then( dothething() );
+    tempID = null;
+    tempElement = null;
+}
---
+++ server.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const express = require('express'),
+      mongodb = require('mongodb'),
+      uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}`,
+      client = new mongodb.MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true}),
+      passport = require('passport'),
+      GitHubStrategy = require('passport-github2').Strategy,
+      app = express();
+let collection = undefined;
+client.connect().then( () => { collection = client.db( 'data' ).collection( 'movies' ) } );
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: '/auth/github/callback' },
+  function(accessToken, refreshToken, profile, done)
+  {
+    User.findOrCreate({ githubId: profile.id },
+    function(err, user) { return done(err, user); });
+  }
+));
+app.get('/login', ( req, res ) =>
+{
+  res.sendFile('index.html', { user: req.user, root: Path2D.join(__dirname, 'public')});
+});
+app.get('/auth/github', passport.authenticate('github', { scope: [ 'user:email' ] }));
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), function(req, res) { res.redirect('/main.html'); });
+app.get('/data', checkAuth, ( req, res ) =>
+{
+  collection.find({ id:mongodb.ObjectId() })
+  .project({ id: 0, items: 1 }).toArray()
+  .then( result => res.json( result ) );
+});
+app.post('/submit', checkAuth, ( req, res ) =>
+{
+  let body = { id: mongodb.ObjectId(), title: req.body.title, genre: req.body.genre, year: req.body.year };
+  collection.updateOne({ id: mongodb.ObjectId() }, 
+                       { $push: { items: body } })
+  .then( result => res.json( result ) );
+});
+app.post('/delete', checkAuth, ( req, res ) =>
+{
+  collection.updateOne({ id: mongodb.ObjectId() },
+                       { $pull: { items: { id: mongodb.ObjectId( req.body.id ) } } })
+  .then( result => res.json( result ) );
+});
+app.post('/update', checkAuth, ( req, res ) =>
+{
+  let changer = { id: mongodb.ObjectId( req.body.id ), title: req.body.title, genre: req.body.genre, year: req.body.year };
+  collection.updateOne({ id: mongodb.ObjectId(), "items.id":mongodb.ObjectId( req.body.id ) },
+                       { $set:  "items.$", changer })
+  .then( result => res.json( result ) );
+});
+function checkAuth( req, res, next ) {
+  if ( allowed.includes(req.path) || req.isAuthenticated()) { return next(); }
+  res.redirect( '/login' ); };
+app.listen( process.env.PORT || 3000 )
