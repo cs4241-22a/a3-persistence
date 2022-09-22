d8eb3ddba6505eb80d067d5019689a4c96480030
r2pen2 <joedobbelaar@gmail.com>
2022-09-21 20:54:29 -0400

    Holy shit project extended lets go I've been working on this since 10am and it's 9 lol

--- public/game.js
+++ public/game.js
@@ -1,58 +1,320 @@
 let spaces = [];
 let mineCount = 0;
+let allMines = [];
 const boardSize = 16;
+let spacesLeft = boardSize * boardSize;
 let boardElement = null;
+let flagMode = false;
+let gameTime = 0;
+let gameOver = false;
+let timerRunning = false;
+let puzzleClicks = [];
+let puzzleFlags = [];
 
 window.onload = () => { 
+    redirectIfNotLoggedIn();
     boardElement = document.getElementById("board");
     loadBoardFromJSON(-1);
+    document.getElementById("flag-mode-toggle").addEventListener("mousedown", () => {
+        flagMode = !flagMode;
+        document.getElementById("flag-mode-toggle").innerHTML = "🚩 " + (flagMode ? "ON" : "OFF");
+    });
+    loadPuzzleList();
+    loadLogoutButton();
+}
+
+function redirectIfNotLoggedIn() {
+    if (!localStorage.getItem("MS:email")) {
+        window.location = "/login";
+    }
+}
+
+function loadLogoutButton() {
+    const email = localStorage.getItem("MS:email");
+    document.getElementById("user-info").innerHTML = "Logged in as: " + email;
+    document.getElementById("logout-button").onclick = () => {
+        window.location = "/";
+    }
+}
+
+function resetBoard() {
+    allMines = [];
+    spaces = [];
+    gameTime = 0;
+    gameOver = false;
+    timerRunning = false;
+    boardElement.innerHTML = "";
+    spacesLeft = boardSize * boardSize;
+    mineCount = 0;
+    puzzleClicks = [];
+    puzzleFlags = [];
+}
+
+function loadPuzzleList() {
+    fetch("/puzzle-list").then((res) => {
+        res.json().then((oldPuzzles) => {
+            if (oldPuzzles) {
+                const puzzleObjects = JSON.parse(oldPuzzles);
+                console.log(puzzleObjects);
+                const puzzleList = document.getElementById("puzzle-list");
+                for (const p of puzzleObjects) {
+                    const puzzleLink = document.createElement("li");
+                    puzzleLink.innerHTML = p.title;
+                    puzzleLink.addEventListener("mousedown", () => {
+                        loadOldPuzzle(p.id);
+                    });
+                    puzzleList.appendChild(puzzleLink);
+                }
+            }
+        })
+    })
+}
+
+function loadOldPuzzle(puzzleId) {
+    resetBoard();
+    loadBoardFromJSON(puzzleId);
+}
+
+function loadWinConditions() {
+    spacesLeft = spacesLeft - mineCount;
+}
+
+function loadMineCounts() {
+    for (const space of spaces) {
+        space.mineCount = space.getAdjacentMines();
+    }
 }
 
 function endGame(won) {
-    if (won) {
-        console.log("You win!");
-    } else {
-        console.log("You lost!");
+    gameOver = true;
+    stopTimer();
+    revealMines(won);
+    document.getElementById("modal-title").innerHTML = won ? "You won!" : "You lost!";
+    document.getElementById("modal-body").innerHTML = won ? "Time: " + gameTime : "Better luck next time!";
+    document.getElementById("modal-button").innerHTML = won ? "Close" : "Close >:(";
+    document.getElementById("modal-button").onclick = () => {
+        $('#modal').modal('hide');
     }
+    $('#modal').modal('show');
+    for (const space of spaces) {
+        if (space.isFlagged) {
+            puzzleFlags.push(space.index);
+        }
+    }
+}
+
+function revealMines(won) {
+    for (const mineIndex of allMines) {
+        const spaceEl = document.getElementById("space-" + mineIndex);
+        spaceEl.classList.add("revealed");
+        spaceEl.innerHTML = '💣';
+        if (won) {
+            spaceEl.classList.add("green");
+        } else {
+            spaceEl.classList.add("mine");
+        }
+    }
+}
+
+function stopTimer() {
+    timerRunning = false;
+}
+
+function startTimer() {
+
+    console.log("Starting timer...");
+
+    const timerP = document.getElementById("time");
+    timerRunning = true;
+
+    function incTime() {
+        if (timerRunning) {
+            timerP.innerHTML = "Time: " + gameTime/100;
+            setTimeout(() => {
+                gameTime += 1;
+                incTime();
+            }, 10)
+        }
+    }
+    
+    incTime();
 }
 
 class Space {
     constructor(i) {
         this.index = i;
         this.isMine = false;
+        this.isRevealed = false;
+        this.mineCount = 0;
+        this.isFlagged = false;
     }
 
     setMine(bool) {
         this.isMine = bool;
     }
-    
-    getIndex() {
-        return this.index;
+
+    getNeighbors() {
+        let neighbors = [
+            {
+                index: this.index - 17,
+                valid: true,
+            },
+            {
+                index: this.index - 16,
+                valid: true,
+            },
+            {
+                index: this.index - 15,
+                valid: true,
+            },
+            {
+                index: this.index - 1,
+                valid: true,
+            },
+            {
+                index: this.index,
+                valid: false,
+            },
+            {
+                index: this.index + 1,
+                valid: true,
+            },
+            {
+                index: this.index + 15,
+                valid: true,
+            },
+            {
+                index: this.index + 16,
+                valid: true,
+            },
+            {
+                index: this.index + 17,
+                valid: true,
+            },
+        ];
+        const onLeft = this.index % 16 === 0;
+        const onRight = (this.index) % 16 === 15;
+        const onTop = this.index < 16;
+        const onBottom = this.index >= 240;
+        if (onLeft) {
+            neighbors[0].valid = false;
+            neighbors[3].valid = false;
+            neighbors[6].valid = false;
+        }
+        if (onRight) {
+            neighbors[2].valid = false;
+            neighbors[5].valid = false;
+            neighbors[8].valid = false;
+        }
+        if (onTop) {
+            neighbors[0].valid = false;
+            neighbors[1].valid = false;
+            neighbors[2].valid = false;
+        }
+        if (onBottom) {
+            neighbors[6].valid = false;
+            neighbors[7].valid = false;
+            neighbors[8].valid = false;
+        }
+        let validNeighbors = [];
+        for (const n of neighbors) {
+            if (n.valid) {
+                validNeighbors.push(n.index);
+            }
+        }
+        return validNeighbors;
     }
 
     place() {
         if (this.isMine) {
             mineCount++;
-            console.log("Placing mine no." + mineCount);
             document.getElementById("total-mines").innerHTML = mineCount;
         }
         const spaceDiv = document.createElement("div");
         spaceDiv.classList.add("space");
-        spaceDiv.addEventListener("mousedown", () => {
-            this.click();
-        })
+        spaceDiv.addEventListener("mousedown", (e) => {
+            this.click(e);
+        });
+        spaceDiv.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+        });
         spaceDiv.setAttribute("id", "space-" + this.index);
         board.appendChild(spaceDiv);
     }
 
-    click() {
+    getAdjacentMines() {
+        const neighbors = this.getNeighbors();
+        let m = 0;
+        for (const n of neighbors) {
+            const neighborObject = spaces[n];
+            if (neighborObject.isMine) {
+                m++;
+            }
+        }
+        return m;
+    }
+
+    reveal(e) {
+        if (this.isRevealed || this.isMine || this.isFlagged) {
+            return;
+        }
+
+        this.isRevealed = true;
         const spaceDiv = document.getElementById("space-" + this.index);
         spaceDiv.classList.add("revealed");
-        if (this.isMine) {
-            spaceDiv.classList.add("mine");
-            spaceDiv.innerHTML = "💣";
-            endGame(false);
+
+        if (this.mineCount > 0) {
+            spaceDiv.innerHTML = this.mineCount > 0 ? this.mineCount.toString() : "";
+            spaceDiv.classList.add("bomb" + this.mineCount.toString());
+        } else {
+            // Totally safe. Reveal neighbors?
+            const neighbors = this.getNeighbors();
+            for (const n of neighbors) {
+                const neighborEl = spaces[n];
+                if (!neighborEl.isMine) {
+                    neighborEl.click(e);
+                }
+            }
         }
+        spacesLeft--;
+        if (spacesLeft <= 0) {
+            endGame(true);
+        }
+    }
+
+    click(e) {
+        if (gameOver) {
+            return;
+        }
+        if (!timerRunning) {
+            startTimer();
+        } 
+        puzzleClicks.push(this.index);
+        if (e.button === 0) {   
+            if (flagMode) {
+                this.toggleFlag();
+            } else {
+                if (this.isMine) {
+                    const spaceDiv = document.getElementById("space-" + this.index);
+                    spaceDiv.classList.add("revealed");
+                    spaceDiv.classList.add("mine");
+                    spaceDiv.innerHTML = "💣";
+                    endGame(false);
+                } else {
+                    this.reveal(e);
+                }
+            }
+        } else if (e.button === 2) {
+            this.toggleFlag();
+        }
+    }
+
+    toggleFlag() {
+        if (this.isRevealed) {
+            return;
+        }
+        this.isFlagged = !this.isFlagged;
+        document.getElementById("space-" + this.index).innerHTML = this.isFlagged ? "🚩" : "";
     }
 }
 
@@ -78,12 +340,51 @@ function loadBoardFromJSON(boardId) {
         }
     }
 
-    let boardData = true;
-    if (boardId === -1) {
-        // Get most recent board from db
+    fetch("/puzzle?id=" + boardId).then((res) => {
+        res.json().then((boardData) => {
+            if (boardData) {
+                const data = JSON.parse(boardData);
+                document.getElementById("title").innerHTML = data.title;
+                parseMines(data.mines);
+                allMines = data.mines;
+                placeSpaces();
+                loadMineCounts();
+                loadWinConditions();
+                checkStatus();
+            }
+        })
+    })
+}
+
+function checkStatus() {
+    const userStatus = "";
+    
+    switch (userStatus) {
+        case "failed":
+            for (const click of userClicks) {
+                spaces[click].reveal({button: 0});
+            }
+            for (const flag of userFlags) {
+                spaces[flag].toggleFlag();
+            }
+            showResult(false);
+            break;
+        case "won":
+            for (const click of userClicks) {
+                spaces[click].reveal({button: 0});
+            }
+            for (const flag of userFlags) {
+                spaces[flag].toggleFlag();
+            }
+            showResult(true);
+            break;
+        default: 
+            break;
     }
-    if (boardData) {
-        parseMines([0, 4]);
-        placeSpaces();
-    }
+}
+
+function showResult(won) {
+    gameOver = true;
+    stopTimer();
+    revealMines(won);
 }
--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 const PORT = 3000;
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+var passport = require("passport");
+const GitHubStrategy = require("passport-github2").Strategy;
+const session = require("express-session");
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { mongo } = require('mongoose');
+const e = require('express');
+const uri = "mongodb+srv://r2pen2:nim26Jmd!@cluster0.clsagc1.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.listen(PORT, () => {
     console.log('Now listening on port ' + PORT);
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static("public"));
+
+
+app.use(session({secret: 'secret', resave: false, saveUninitialized: false, cookie: {httpOnly: true, secure: true, maxAge: 60 * 1000,},}))
+
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'minesweeperdle',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public", "index.html"));
@@ -16,6 +44,299 @@ app.get("/", (req, res) => {
 
 app.get("/play", (req, res) => {
     res.sendFile(path.join(__dirname, "/public", "game.html"));
+});
+
+app.get("/puzzle", (req, resp) => {
+    const boardId = req.query.id;
+    // Fetch board from DB
+
+    console.log(boardId);
+
+    if (boardId == -1) {
+        // Fetch most recent
+        client.connect((err, client) => {
+            if (err) {
+                throw err;
+            } else {
+                const db = client.db("db1");
+                db.collection("puzzles").findOne({}, {sort:{'$natural':-1}}, (err, res) => {
+                    if (err) {
+                        throw err;
+                    } else {
+    
+                        const body = {
+                            mines: res.mines,
+                            title: res.title
+                        }
+                        resp.json(JSON.stringify(body));
+                        resp.end();
+                    }
+                })
+            }
+        })
+    } else {
+        console.log("Loading old board...")
+        client.connect((err, client) => {
+            if (err) {
+                throw err;
+            } else {
+                const db = client.db("db1");
+                db.collection("puzzles").findOne({"_id": new ObjectId(boardId)}, {}, (err, res) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        const body = {
+                            mines: res.mines,
+                            title: res.title
+                        }
+                        resp.json(JSON.stringify(body));
+                        resp.end();
+                    }
+                })
+            }
+        })
+    }
+})
+
+app.get("/puzzle-list", (req, resp) => {
+    client.connect((err, client) => {
+        if (err) {
+            throw err;
+        } else {
+            const db = client.db("db1");
+            db.collection("puzzles").find({}, {sort:{'$natural':-1}}).limit(25).toArray((err, res) => {
+                if (err) {
+                    throw err;
+                } else {
+                    const body = [];
+                    for (const item of res) {
+                        body.push({id: item._id, title: item.title})
+                    }
+                    resp.json(JSON.stringify(body));
+                    resp.end();
+                }
+            })
+        }
+    })
+})
+
+function generateMineLayout() {
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    let mines = [];
+    for (let i = 0; i < 32; i++) {
+        let newSpace = false;
+        while (!newSpace) {
+            const randomSpace = getRandomInt(255);
+            if (!mines.includes(randomSpace)) {
+                newSpace = true;
+                mines.push(randomSpace);
+            }
+        }
+    }
+    return mines;
+}
+
+function getPuzzleTitle(date) {
+    return "Minesweeperdle: " + getDateString(date);
+}
+
+function getDateString(date) {
+    const d = new Date(date)
+    const day = d.getUTCDate();
+    const month = d.getMonth();
+    const year = d.getFullYear();
+
+    var monthString = ""
+    switch (month) {
+        case 0:
+            monthString = "January";
+            break;
+        case 1:
+            monthString = "February";
+            break;
+        case 2:
+            monthString = "March";
+            break;
+        case 3:
+            monthString = "April";
+            break;
+        case 4:
+            monthString = "May";
+            break;
+        case 5:
+            monthString = "June";
+            break;
+        case 6:
+            monthString = "July";
+            break;
+        case 7:
+            monthString = "August";
+            break;
+        case 8:
+            monthString = "September";
+            break;
+        case 9:
+            monthString = "October";
+            break;
+        case 10:
+            monthString = "November";
+            break;
+        case 11:
+            monthString = "December";
+            break;
+        default:
+            monthString = "";
+            break;
+    }
+
+    return monthString + " " + day + ", " + year;
+}
+
+function createNewPuzzleOnDb(puzzleDate) {
+    const mines = generateMineLayout();
+    const puzzleTitle = getPuzzleTitle(puzzleDate);
+    var puzzle = {
+        title: puzzleTitle,
+        mines: mines
+    }
+    client.connect((err, client) => {
+        if (err) {
+            throw err;
+        } else {
+            const db = client.db("db1");
+            db.collection("puzzles").insertOne(puzzle, (err, res) => {
+                if (err) {
+                    throw err;
+                } else {
+                    console.log("New puzzle sent to database!");
+                }
+            })
+        }
+    })
+    setTimeout(() => {
+        createNewPuzzleOnDb(new Date());
+    }, 86400000); // Make a new puzzle in 24 hours
+    client.close();
+}
+
+app.get("/get-data", (req, res) => {
+
+})
+
+var launchTime = new Date();
+var msUntilSend = new Date(launchTime.getFullYear(), launchTime.getMonth(), launchTime.getDate(), 9, 0, 0, 0) - launchTime;
+if (msUntilSend < 0) {
+    msUntilSend += 86400000; // it's after 9am
+}
+setTimeout(() => {
+    createNewPuzzleOnDb(new Date());
+}, msUntilSend);
+
+
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public", "login.html"));
+});
+
+app.get('/new-user', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public", "newUser.html"));
+});
+
+app.post("/new-user", (req, resp) => {
+    const data = req.body;
+    client.connect((err, client) => {
+        if (err) {
+            throw err;
+        } else {
+            const db = client.db("db1");
+            db.collection("users").findOne({"email": data.email}, {}, (err, res) => {
+                if (err) {
+                    throw err;
+                } else {
+                    if (res) {
+                        const body = {
+                            error: true
+                        }
+                        console.log("User already exists.");
+                        resp.json(JSON.stringify(body));
+                        resp.end();
+                    } else {
+                        bcrypt.hash(data.password, saltRounds, function(err, hash) {
+                            const newUser = {
+                                email: data.email,
+                                password: hash,
+                                saveData: data.progressCheck
+                            }
+                            db.collection("users").insertOne(newUser, (err, res) => {
+                                if (err) {
+                                    throw err;
+                                } else {
+                                    console.log("New user sent to database!");
+                                    const body = {
+                                        error: false,
+                                        email: data.email,
+                                    }
+                                    resp.json(JSON.stringify(body));
+                                    resp.end();
+                                }
+                            })
+                        });
+                    }
+                }
+            })
+        }
+    })
+})
+
+app.post("/existing-user", (req, resp) => {
+    const data = req.body;
+    client.connect((err, client) => {
+        if (err) {
+            throw err;
+        } else {
+            const db = client.db("db1");
+            db.collection("users").findOne({"email": data.email}, {}, (err, res) => {
+                if (err) {
+                    throw err;
+                } else {
+                    if (!res) {
+                        const body = {
+                            error: true
+                        }
+                        resp.json(JSON.stringify(body));
+                        resp.end();
+                    } else {
+                        const hash = res.password;
+                        bcrypt.compare(data.password, hash, (err, bcryptres) => {
+                            if (bcryptres) {
+                                const body = {
+                                    error: false,
+                                    email: data.email,
+                                }
+                                resp.json(JSON.stringify(body));
+                                resp.end();
+                            } else {
+                                const body = {
+                                    error: true
+                                }
+                                resp.json(JSON.stringify(body));
+                                resp.end();
+                            }
+                        })
+                    }
+                }
+            })
+        }
+    })
+})
+
+
+app.get('/sign-in', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public", "existingUser.html"));
 });
 
 app.get("*", (req, res) => {
