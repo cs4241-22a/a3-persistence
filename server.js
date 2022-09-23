7f42b5c54e46f44811d4f7dce913f459e8765b0f
Dillon McCarthy <dillonmccarthy423@gmail.com>
2022-09-23 15:12:05 -0400

    port binding for heroku

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ app.use( serveStatic( 'views'  ) )
 app.use( express.json() )
 app.use(timeout('5s'))
 
+// For avoidong Heroku $PORT error
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 // vvv REMOVE WHEN ADDED TO GLITCH vvv
 const USER="Dillon"
 const PASS="ba11islife"
@@ -149,5 +159,3 @@ app.use( function( req,res,next) {
 
 
 app.use( serveStatic( 'public' ) )
-  
-app.listen( process.env.PORT | 3000 )
