72cf00851cae64e06114bbe5bfde0d98b0fa2505
asaduakas <arman.sdkas@gmail.com>
2022-09-24 20:37:29 -0400

    Github authentication works, moving to glitch again

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express"),
   mongodb = require("mongodb"),
   cookie = require("cookie-session"),
   dotenv = require("dotenv"),
+  helmet = require("helmet"),
+  passport = require("passport"),
+  session = require("express-session"),
+  GitHubStrategy = require("passport-github").Strategy,
   app = express();
 
 dotenv.config();
@@ -53,60 +57,98 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.static("views"));
 
+//To check if the user is authenticated
+const checkAuthenticated = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/login.html')
+  }
+}
+
 //=========AUTHENTICATION==========
-app.post("/login", (req, res) => {
-	// express.urlencoded will put your key value pairs
-	// into an object, where the key is the name of each
-	// form field and the value is whatever the user entered
-	console.log(req.body);
-	const inputUname = req.body.uname
-	const inputPword = req.body.uname
+//Passport stuff
+let gitId = null
+app.use(helmet())
 
-	// below is *just a simple authentication example*
-	// for A3, you should check username / password combos in your database
+app.use(
+  session({secret: 'secret', resave: false, saveUninitialized: false, cookie: {httpOnly: true, secure: true, maxAge: 60 * 1000,},})
+)
 
-	if (req.body.uname === collection.find(req.body.uname)) {
-		// define a variable that we can check in other middleware
-		// the session object is added to our requests by the cookie-session middleware
-		req.session.login = true;
+app.use(passport.initialize())
+app.use(passport.session())
 
-		//res.redirect("main.html");
-		console.log("log in successful!")
-	} else {
-		// password incorrect, redirect back to login page
-		res.sendFile(__dirname + "/views/index.html");
-	}
-});
+passport.serializeUser(function (user, callback) {
+  callback(null, user.id);
+})
 
+passport.deserializeUser(function (id, callback) {
+  callback(null, id);
+})
 
-//Add a user
-app.post("/signUp", (req, res) => {
-	console.log(req.body);
-	collection.insertOne(req.body).then((result) => res.json(result));
-  });
+passport.use(
+  new GitHubStrategy({
+      clientID: process.env.ID,
+      clientSecret: process.env.SECRET,
+      callbackURL: 'https://a3-arman-saduakas.glitch.me/auth/github/callback',
+    },
+    function (accessToken, refreshToken, profile, callback) {
+      callback(null, profile);
+    }
+  )
+)
 
-//Remove a user
-app.post("/delete", (req, res) => {
-  collection
-    .deleteOne({ _id: mongodb.ObjectId(req.body._id) })
-    .then((result) => res.json(result));
-});
+app.get('/auth/github', passport.authenticate('github'));
+app.get(
+    '/auth/github/callback',
+    passport.authenticate('github', { failureRedirect: '/login.html' }),
+    function (req, res) {
+      githubid = req.user.id;
+      res.redirect('/main.html')
+  }
+)
+
+//Routes
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/login.html')
+})
+
+app.get('/main', checkAuthenticated, function (req, res) {
+  res.sendFile(__dirname + '/views/main.html')
+})
+
+app.get('/logout', function (req, res) {
+  res.redirect('/')
+})
+
+// //Add a user
+// app.post("/signUp", (req, res) => {
+// 	console.log(req.body);
+// 	collection.insertOne(req.body).then((result) => res.json(result));
+//   });
+
+// //Remove a user
+// app.post("/delete", (req, res) => {
+//   collection
+//     .deleteOne({ _id: mongodb.ObjectId(req.body._id) })
+//     .then((result) => res.json(result));
+// });
 
 //Update a user
-app.post("/update", (req, res) => {
-  collection
-    .updateOne(
-      { _id: mongodb.ObjectId(req.body._id) },
-      { $set: { name: req.body.name } }
-    )
-    .then((result) => res.json(result));
-});
+// app.post("/update", (req, res) => {
+//   collection
+//     .updateOne(
+//       { _id: mongodb.ObjectId(req.body._id) },
+//       { $set: { name: req.body.name } }
+//     )
+//     .then((result) => res.json(result));
+// });
 
 app.use(function(req,res,next){
 	if(req.session.login === true)
 		next()
 	else
-		res.sendFile( __dirname + '/views/index.html')
+		res.sendFile( __dirname + '/views/login.html')
 })
 
 app.listen(3000, () => console.log("Server started succesfully!"));
