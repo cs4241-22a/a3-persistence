69604a7a93fa3161e010d4d03fb5d5f45b95e246
may-s-d <may.s.dong@gmail.com>
2022-09-24 20:29:19 -0400

    Add cookie-session middleware

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const mongodb = require("mongodb")
 const hbs = require("express-handlebars").engine
 const ObjectId = require('mongodb').ObjectId;
 
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.engine("handlebars", hbs())
+app.set("view engine", "handlebars")
+app.set("views", "views")
+
 // favicon middleware; serves favicon
 const favicon = require('serve-favicon')
 const path = require('path')
@@ -13,19 +20,10 @@ app.use(favicon(path.join(__dirname, 'public', 'resources', 'favicon.png')))
 const compression = require('compression')
 app.use(compression())
 
-app.use(express.static("public"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.engine("handlebars", hbs())
-app.set("view engine", "handlebars")
-app.set("views", "views")
-
-
 const uri = "mongodb+srv://mdong:Bacon1A3@cluster0.qkeu5vg.mongodb.net/"
 const client = new mongodb.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:true })
 
 let collection = null;
-let currentId = null;
 
 // connect
 client.connect()
@@ -37,19 +35,17 @@ client.connect()
     return collection.find({}).toArray()
 })
 
-// i have no idea what this does, but everything breaks when i remove it so it'll stay
-app.use((req, res, next) => {
-    if (collection !== null) {
-      next()
-    }
-    else {
-      res.status(503).send()
-    }
-})
+// cookie-session middleware
+const cookie  = require('cookie-session');
+app.use(cookie({
+    name: 'session',
+    keys: ['key1', 'key2']
+}))
 
 // first page
 app.get("/", (req,res) => {
-    res.render("index", { msg:"", layout:false })
+    if (req.session.login === true) { res.redirect("todo") }
+    else { res.render("index", { msg:"", layout:false }) }
 })
 
 // login
@@ -63,8 +59,9 @@ app.post("/login", async(req, res) => {
         const usernameFound = data[0].length != 0;
         const accountFound = data[1].length != 0;
         if (accountFound) {
-            currentId = data[0][0]._id;
-            res.redirect("/todo");
+            req.session.id = data[0][0]._id.toString();
+            req.session.login = true;
+            res.redirect("/todo_login");
         }
 
         else if (!usernameFound) {
@@ -73,19 +70,38 @@ app.post("/login", async(req, res) => {
                 password: pass,
                 todolist: []
             });
-            currentId = ObjectId(result.insertedId);
+            req.session.id = result.insertedId;
+            req.session.login = true;
             res.redirect("/todo_new");
         }
 
         else {
-            currentId = null;
+            req.session.id = null;
+            req.session.login = false;
             res.render("index", { msg:"login failed, please try again", layout:false })
         }
     })
 })
 
+// redirects unauthorized users to login
+app.use(function(req, res, next) {
+    if (req.session.login === true) { next() }
+    else { 
+        res.sendFile(path.join(__dirname, "views", "index"), function(err) {
+            if (err) {
+                res.status(err.status).end();
+            }
+        });
+    }
+})
+
 // send todo page
 app.get("/todo", (req, res) => {
+    res.render("todo", { msg:"", layout:false })
+})
+
+// send todo page on login
+app.get("/todo_login", (req, res) => {
     res.render("todo", { msg:"success you have logged in", layout:false })
 })
 
@@ -97,7 +113,7 @@ app.get("/todo_new", (req, res) => {
 // add new todo item
 app.post("/add", (req, res) => {
     collection.updateOne(
-      { _id: currentId },
+      { _id: ObjectId(req.session.id) },
       { $push: { todolist: req.body }}
     )
     .then (result => res.json(result))
@@ -106,7 +122,7 @@ app.post("/add", (req, res) => {
 // deletes one todo item
 app.post("/delete", (req, res) => {
     collection.updateOne(
-        { _id: currentId },
+        { _id: ObjectId(req.session.id) },
         { $pull: { todolist: { iid: req.body.iid } }}
     )
     .then (result => res.json(result))
@@ -115,7 +131,7 @@ app.post("/delete", (req, res) => {
 // edits todo item
 app.post("/edit", (req, res) => {
     collection.updateOne(
-        { _id: currentId, "todolist.iid": req.body.iid },
+        { _id: ObjectId(req.session.id), "todolist.iid": req.body.iid },
         { $set: { "todolist.$.title": req.body.title } }
     )
     .then (result => res.json(result))
@@ -123,12 +139,11 @@ app.post("/edit", (req, res) => {
 
 // get todolist
 app.get("/todolist", (req, res) => {
-    collection.find({_id: currentId}, {_id: 0, todolist: 1}).toArray()
-    .then(result => {
-        res.json(result[0].todolist)
-    })
+    collection.find({_id: ObjectId(req.session.id) }, {_id: 0, todolist: 1}).toArray()
+    .then(result => res.json(result[0].todolist))
 })
 
+app.use(express.static("public"))
 const port = 3000;
 const listener = app.listen(port, () => {
   console.log("Your app is listening on port " + listener.address().port);
