23fca5fda63a904718f8faccadf8fc68effe3645
Colb131 <cfrechette@wpi.edu>
2022-09-25 15:03:15 -0400

    Fixed server

--- server.js
+++ server.js
@@ -23,6 +23,24 @@ passport.deserializeUser(function(obj, done) {
     done(null, obj);
 });
 
+passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: process.env.GITHUB_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+
+            // To keep the example simple, the user's GitHub profile is returned to
+            // represent the logged-in user.  In a typical application, you would want
+            // to associate the GitHub account with a user record in your database,
+            // and return that user instead.
+            return done(null, profile);
+        });
+    }
+));
+
 app.use( express.static('public') )
 app.use( express.static('views'))
 app.use( express.json() )
@@ -41,23 +59,7 @@ const uri = 'mongodb+srv://'+process.env.USERNAME+':'+process.env.PASSWORD+'@'+p
 const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology:true })
 let collection = null
 
-passport.use(new GitHubStrategy({
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: process.env.GITHUB_CALLBACK_URL
-    },
-    function(accessToken, refreshToken, profile, done) {
-        // asynchronous verification, for effect...
-        process.nextTick(function () {
 
-            // To keep the example simple, the user's GitHub profile is returned to
-            // represent the logged-in user.  In a typical application, you would want
-            // to associate the GitHub account with a user record in your database,
-            // and return that user instead.
-            return done(null, profile);
-        });
-    }
-));
 
 
 app.use( cookie({
