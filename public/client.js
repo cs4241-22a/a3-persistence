912f5009eaefc255278f93ce999f96ce57c8e8a1
Colb131 <cfrechette@wpi.edu>
2022-09-25 22:07:24 -0400

    added console log

--- public/client.js
+++ public/client.js
@@ -60,6 +60,8 @@ const remove = function( e ) {
         json = { climbName: climbName.value },
         body = JSON.stringify( json )
 
+    console.log('calling remove climb name with' + climbName)
+
     climbName.value = ''
 
     fetch( '/removeClimb', {
