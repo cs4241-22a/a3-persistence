cdf6e44a71c1aef4774cb1b1faf5a0312ef7d6be
hyoder <hryoder@wpi.edu>
2022-09-26 13:01:10 -0400

    commit

--- server.js
+++ server.js
@@ -78,4 +78,7 @@ function checkAuth( req, res, next )
   if ( req.isAuthenticated()) { return next(); }
   res.render( "index", { msg: "", layout: false } );
 };
-app.listen( process.env.PORT || 51201 );
+app.listen(process.env.PORT || 3000, function()
+{
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
