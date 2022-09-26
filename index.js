d24bc8f3f28291c09208e6b8e3bad9d5ba8f65a1
Michael Emerson <emerson.michael12@gmail.com>
2022-09-26 05:42:58 -0400

    heroku callback

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ passport.deserializeUser(function (user, done) {
 passport.use(new GitHubStrategy({
     clientID: process.env.GH_CLIENT_ID,
     clientSecret: process.env.GH_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/github/callback"
+    callbackURL: "https://webware-a3.herokuapp.com/auth/github/callback"
 }, function (accessToken, refreshToken, profile, done) {
     return done(null, profile);
 }));
