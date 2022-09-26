44c68ad3136e9fa2ed40aece5cac1a3a9d4222ae
Eri Kim <59063929+IeKimI@users.noreply.github.com>
2022-09-26 09:01:37 -0400

    Update passport.js

--- passport.js
+++ passport.js
@@ -9,7 +9,6 @@ passport.deserializeUser((user, done) => {
     done(null, user)
 })
 
-// noinspection JSCheckFunctionSignatures
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
