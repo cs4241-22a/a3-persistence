4948cf3e0cbfe6cbb939886cd95f0ffaf5eb7497
npollock14 <ndpollock@wpi.edu>
2022-09-26 04:55:37 -0400

    new port

--- server.improved.js
+++ server.improved.js
@@ -418,6 +418,6 @@ app.get("*", (req, res) => {
   res.sendFile(__dirname + "/public" + req.url);
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
