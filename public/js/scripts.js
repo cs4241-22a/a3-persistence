c91780de83f36e1917f99f45bf457ce31b6be1b7
Othniel9 <othnielbondah@gmail.com>
2022-09-25 20:46:22 -0400

    All done

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,25 @@
-let Todo = []
+
+
+const Todo = []
 const task = document.getElementById('to_do')
 const date = document.querySelector('#date')
 const priority = document.querySelector('#priority')
 const results = document.getElementById("results")
 const add = document.querySelector('#add')
+let json
+let table_len
+
+fetch("/currentdb", {
+    method: 'GET',
+    headers: {'Content-Type': 'application/json'}
+}).then(async (response) => {
+    const data = await response.json()
+    data.forEach(item => {
+        Todo.push(item)
+            table_len = (results.rows.length) - 1
+            results.insertRow(table_len + 1).innerHTML = "<tbody><tr id='row" + table_len + "'><td id='task_row" + table_len + "'>" + item.Task + "</td><td id='date_row" + table_len + "'>" + item.Date + "</td><td id='priority_row" + table_len + "'>" + item.Priority + "</td><td><input type='button' class='btn-primary' id='edit_button" + table_len + "' value='Edit' class='edit' onclick='edit_row(" + table_len + ")'> <input type='button' class='btn-primary' id='save_button" + table_len + "' value='Save' class='save' onclick='save_row(" + table_len + ")'> <input type='button' class='btn-primary' value='Delete' class='delete' onclick='delete_row(" + table_len + ")'></td></tr></tbody>"
+    })
+})
 
 function isEmpty(){
     if (task.value === "" || date.value === "" || task.value === null || date.value === null || priority.value === "none" || priority.value === null){
@@ -20,26 +36,25 @@ add.onclick = function (event){
     }
 }
 function add_row(){
-    const json = {
+
+    // table_len = (results.rows.length) - 1
+    results.insertRow(table_len + 1).innerHTML= "<tbody><tr id='row"+table_len+"'><td id='task_row"+table_len+"'>"+task.value+"</td><td id='date_row"+table_len+"'>"+date.value+"</td><td id='priority_row"+table_len+"'>"+priority.value+"</td><td><input type='button' class='btn-primary' id='edit_button"+table_len+"' value='Edit' class='edit' onclick='edit_row("+table_len+")'> <input type='button' class='btn-primary' id='save_button"+table_len+"' value='Save' class='save' onclick='save_row("+table_len+")'> <input type='button' class='btn-primary' value='Delete' class='delete' onclick='delete_row("+table_len+")'></td></tr></tbody>"
+    json = {
         Task: task.value,
         Date: date.value,
         Priority: priority.value
     }
-
-
-    const table_len = (results.rows.length)
-    results.insertRow(table_len).innerHTML= "<tr id='row"+table_len+"'><td id='task_row"+table_len+"'>"+task.value+"</td><td id='date_row"+table_len+"'>"+date.value+"</td><td id='priority_row"+table_len+"'>"+priority.value+"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit' class='edit' onclick='edit_row("+table_len+")'> <input hidden type='button' id='save_button"+table_len+"' value='Save' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete' class='delete' onclick='delete_row("+table_len+")'></td></tr>"
-
     Todo.push(json)
     task.value = ""
     date.value = ""
     priority.value = ""
-    // fetch('/add', {
-    //     method: 'POST',
-    //     headers: {'Content-Type': 'application/json'},
-    //     body: JSON.stringify(json)
-    // })
-    //     .then(response => response.json())
+
+    fetch('/add', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(json)
+    })
+        .then(response => response.json())
 }
 function edit_row(index)
 {
@@ -55,12 +70,18 @@ function edit_row(index)
     let priority_data=priority.innerHTML;
 
     task.innerHTML="<input type='text' id='task_text"+index+"' value='"+task_data+"'>";
-    date.innerHTML="<input type='text' id='date_text"+index+"' value='"+date_data+"'>";
-    priority.innerHTML="<input type='text' id='priority_text"+index+"' value='"+priority_data+"'>";
+    date.innerHTML="<input type='date' id='date_text"+index+"' value='"+date_data+"'>";
+    priority.innerHTML="<select id='priority_text"+index+"''><option value='"+priority_data+"'>"+priority_data+"</option><option>High</option><option>Medium</option><option>Low</option></select>"
+
+    fetch('/edit', {
+        method:'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body:JSON.stringify(Todo[index])
+    })
+        .then( response => response.json())
 }
 
 function save_row(index){
-
     let task_val=document.getElementById("task_text"+index).value;
     let date_val=document.getElementById("date_text"+index).value;
     let priority_val=document.getElementById("priority_text"+index).value;
@@ -72,28 +93,41 @@ function save_row(index){
     document.getElementById("edit_button"+index).style.display="block";
     document.getElementById("save_button"+index).style.display="none";
 
- //   fetch('/edit', {
-//     method:'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body:JSON.stringify( json )
-// })
-//     .then( response => response.json())
+    for (let i = 0; i < Todo.length; i++) {
+        if (i === index){
+            Todo[i].Task = task_val
+            Todo[i].Date = date_val
+            Todo[i].Priority = priority_val
+        }
+    }
+
+    fetch('/save', {
+        method:'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body:JSON.stringify(Todo[index])
+    })
+        .then( response => response.json())
+
+
 }
 
 
-function delete_row(){
-    //document.getElementById("row"+index).outerHTML="";
+function delete_row(index){
+    fetch('/delete', {
+        method:'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body:JSON.stringify( Todo[index] )
+    })
+        .then( response => response.json())
+    for (let j = 0; j < Todo.length; j++) {
+        if (index === j){
+            Todo.splice(j,1)
+        }
+    }
     let i = event.target.parentNode
     let tr = i.parentNode
     tr.parentNode.removeChild(tr)
 
-    // fetch('/delete', {
-//     method:'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body:JSON.stringify( json )
-// })
-//     .then( response => response.json())
-// }
 }
 
 
