7a72e7acc326e920f55a235ce93149a1826b2332
kaelinpanneton <68758878+kaelinpanneton@users.noreply.github.com>
2022-09-28 11:18:48 -0400

    sync with glitch server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require( 'express' ),
+      mongodb = require( 'mongodb' ),
+      app = express()
+
+app.use( express.static('public') )
+app.use( express.json() )
+
+const uri = 'mongodb+srv://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST
+
+const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology:true })
+let collection = null
+
+client.connect()
+  .then( () => client.db( 'testdb' ).collection( 'test collection' ) )
+  .then( __collection => {
+    collection = __collection
+    return collection.find({ }).toArray()
+  })
+  .then( console.log )
+  
+// route to get all docs
+app.get( '/', (req,res) => {
+  if( collection !== null ) {
+    // get array and pass to res.json
+    collection.find({ }).toArray().then( result => res.json( result ) )
+  }
+})
+  
+app.listen( 3000 )
