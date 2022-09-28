5c113d42bfa3c0e90c840bf85114a12387393fe1
Glitch (a3-jonathan-palmieri) <none>
2022-09-28 03:55:55 +0000

    🛎️🥥 Updated with Glitch

---
+++ public/home.js
@@ -0,0 +1,47 @@
+const saveForm = document.getElementById("save");
+const email = document.getElementById("email");
+let link = "";
+
+window.onload = (event) => {
+  console.log('page is fully loaded');
+  email.innerHTML = "faceitlvl2@faceit.com";
+  link = window.location.href;
+  link = link.substring(link.lastIndexOf("/")+1);
+  getData(link);
+};
+
+saveForm.addEventListener("click", async function(event) {
+
+  console.log(link);
+  event.preventDefault();
+
+  let JSONObject =
+        {"username": link,
+         "password": "",
+         "firstname": document.getElementById("firstname").value,
+         "lastname": document.getElementById("lastname").value,
+         "discordID": document.getElementById("discordID").value,
+         "rank": document.getElementById("rank").value,
+         "steamID": document.getElementById("steamID").value
+        }
+
+  
+  const response = await fetch( '/update', {
+    method:  'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body:    JSON.stringify(JSONObject)
+  })
+  console.log("fetch");
+})
+
+async function getData(name){
+  const response = await fetch("/getData", {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({username: name})
+  })  
+  
+  const responseJSON = await response.json();
+  email.innerHTML = responseJSON.username;
+  console.log(responseJSON);
+}
