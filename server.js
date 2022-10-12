1422dea41cd4d6735f95c3cfcdee797cf448215e
kaelinpanneton <68758878+kaelinpanneton@users.noreply.github.com>
2022-10-12 16:36:53 -0400

    delete files

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ const express = require( 'express' ),
       bodyParser  = require( 'body-parser' ),
       timeout  = require( 'connect-timeout' ),
       response  = require( 'response-time' ),
-      hbs  = require( 'express-handlebars' ),
+      hbs  = require( 'express-handlebars' ).engine,
       app = express()
 
 app.use( express.static('public') )
+app.use(express.static('views'));
 app.use( express.json() )
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+app.engine( 'handlebars',  hbs() )
+app.set(    'view engine', 'handlebars' )
+app.set(    'views',       './views' )
 
 require('dotenv').config()
 
@@ -25,61 +30,74 @@ const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnified
 let collection = null
 
 client.connect()
-  .then( () => client.db( 'testdb' ).collection( 'testcollection' ) )
+  .then( () => client.db( 'Assignment4' ).collection( 'gameHold' ) )
   .then( __collection => {
     collection = __collection
     return collection.find({ }).toArray()
   })
   .then( console.log )
+
+  app.use( (req,res,next) => {
+    if( collection !== null ) {
+      next()
+    }else{
+      res.status( 503 ).send()
+    }
+  })
   
-// route to get all docs
-app.get( '/', (req,res) => {
-  if( collection !== null ) {
-    // get array and pass to res.json
-    collection.find({ }).toArray().then( result => res.json( result ) )
-  }
-})
 
-app.use( (req,res,next) => {
-  if( collection !== null ) {
-    next()
-  }else{
-    res.status( 503 ).send()
-  }
-})
-
-app.post('/form', function(req,res){
+app.post('/login', async function(req,res){
   const username = req.body.username;
-  const useremail = req.body.useremail;
-  const gameTitle = req.body.gameTitle;
-  const gameSystem = req.body.gameSystem;
-  const currentDate = req.body.currentDate;
-
-  const data = {
-      "name": username,
-      "email": useremail,
-      "gameTitle": gameTitle,
-      "gameSystem": gameSystem,
-      "currentDate": currentDate
+  const password = req.body.password;
+  const acceptPassword = await collection.findOne({"username": username})
+  if (acceptPassword === password ) {
+    req.session.login = true
+    res.render('index')
+    // res.redirect('index');
   }
 
-  db.collection('details').insertOne(data,function(err, collection){
-    if (err) throw err;
-    console.log("Record inserted Successfully");
-          
-});
-
-return res.redirect('signup_success.html');
+  else {
+    req.session.login = false
+    res.render('form', { msg:'Login failed.', layout:false})
+  }
 })
+
+// app.post('/form', function(req,res){
+//   const username = req.body.username;
+//   const useremail = req.body.useremail;
+//   const gameTitle = req.body.gameTitle;
+//   const gameSystem = req.body.gameSystem;
+//   const currentDate = req.body.currentDate;
+
+//   const data = {
+//       "name": username,
+//       "email": useremail,
+//       "gameTitle": gameTitle,
+//       "gameSystem": gameSystem,
+//       "currentDate": currentDate
+//   }
+
+//   db.collection('details').insertOne(data,function(err, collection){
+//     if (err) throw err;
+//     console.log("Record inserted Successfully");
+          
+// });
+
+// return res.redirect('index.html');
+// })
 
 app.use( cookie({
   name: 'session',
-  keys: ['key1', 'key2']
+  keys: ['59765', '26438']
 }))
 
 //pages
 app.get( '/', (req,res) => {
   res.render( 'index', { msg:'', layout:false })
+})
+
+app.get( '/login', ( req, res) => {
+  res.render( 'form', { msg:'', layout:false })
 })
 
 app.listen( 3000 )
