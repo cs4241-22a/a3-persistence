503b0cd5378a74812aabd49f9ad84deaf6a5fa2c
Nappa22428 <Nappa22428@gmail.com>
2022-10-13 17:59:19 -0400

    maybe it works

--- server.improved.js
+++ server.improved.js
@@ -1,11 +1,90 @@
+const getUser = require('./login');
+
 const http = require( 'http' ),
       fs   = require( 'fs' ),
       // IMPORTANT: you must run `npm install` in the directory for this assignment
       // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
+      mime = require( 'mime' ), 
+      express = require('express'),
+      dotenv = require('dotenv'),
+      mongoose = require('mongoose'),
+      mongodb = require('mongodb'),
+      bodyParser = require("body-parser"),
+      cookieParser = require("cookie-parser"),
+      session = require("express-session"),
+      passport = require("passport"),
       dir  = 'public/',
       port = 3000
+     
+dotenv.config()
 
+const app = express()
+
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+const auth = (req, res, next) => {
+  if (req.session.email) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+};
+
+app.use(express.static("public"));
+app.use(express.static("./"));
+
+app.use(cookieParser("" + process.env.COOKIE_SECRET));
+app.use(
+  session({
+    secret: "" + process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: false,
+      //cookie expires in 30 days
+      maxAge: 1000 * 30 * 24 * 60 * 60,
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, cb) {
+  cb(null, { email: user.id });
+});
+
+passport.deserializeUser(function (email, cb) {
+  cb(null, email);
+});
+
+const uri = `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@cluster0.uyte9vz.mongodb.net/test`;
+mongoose.connect(uri).catch((err)=>{console.log(err)})
+const client = new mongodb.MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: mongodb.ServerApiVersion.v1,
+});
+
+let collection = null;
+
+client
+  .connect()
+  .then(() => {
+    return client.db("test").collection("users");
+  })
+  .then((collection) => {
+    collection = collection;
+
+    return collection.find({}).toArray();
+  })
+  .then(console.log);
 const appdata = [
   {
     game: "Tetris",
@@ -21,71 +100,24 @@ const appdata = [
   },
 ];
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
-  }
-})
+//getUser.loginUser({email: "bruhh",password: "something"})
 
-const handleGet = function (request, response) {
-  const filename = dir + request.url.slice(1);
+app.post("/login", async (req, res) => {
+  loginUser(req.body)
+    .then((data, err) => {
+      req.session.email = data.email;
+      res.status(200);
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(401);
+    });
+});
+app.post("/logout", async (req, res) => {
+  req.session.destroy();
 
-  if (request.url === "/") 
-    {
-      sendFile(response, "public/index.html");
-    } else if (request.url === "/public/js/scripts.js") 
-    {
-      sendFile(response, "public/js/scripts.js");
-    } else if (request.url === "/public/css/style.css") 
-    {
-      sendFile(response, "public/css/style.css");
-    } else if (request.url === "/submit") 
-    {
-      response.writeHeader(200, { "Content-Type": "text/plain" });
-      response.end(JSON.stringify(appdata));
-    } else 
-    {
-      sendFile(response, filename);
-    }
-};
-const handlePost = function( request, response ) {
-  let dataString = ''
+  res.redirect("/");
+});
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
-
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-    let jsonData = JSON.parse(dataString);
-    console.log(jsonData + "1");
-    
-     if (request.url === "/submit") {
-      console.log(jsonData);
-      appdata.push(jsonData); 
-    }
-    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
-    response.end(JSON.stringify(appdata));
-  });
-};
-
-const sendFile = function (response, filename) {
-  const type = mime.getType(filename);
-
-  fs.readFile(filename, function (err, content) {
-    // if the error = null, then we've loaded the file successfully
-    if (err === null) {
-      // status code: https://httpstatuses.com
-      response.writeHeader(200, { "Content-Type": type });
-      response.end(content);
-    } else {
-      // file not found, error code 404
-      response.writeHeader(404);
-      response.end("404 Error: File Not Found");
-    }
-  });
-};
-
-server.listen(process.env.PORT || port);
+app.listen(process.env.PORT || port);
